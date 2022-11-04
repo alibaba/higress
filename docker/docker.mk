0b78945a3f21e4b33b4888a9618722d858dfbe1f
zty98751 <zty98751@alibaba-inc.com>
2022-11-04 17:40:09 +0800

    add copyright

--- docker/docker.mk
+++ docker/docker.mk
@@ -1,3 +1,17 @@
+## Copyright 2018 Istio Authors
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
 docker.higress: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION} --build-arg HUB=${HUB}
 docker.higress: $(OUT_LINUX)/higress
 docker.higress: docker/Dockerfile.higress
