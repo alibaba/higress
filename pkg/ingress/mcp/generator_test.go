1169a79dc53d143f431a30ac3a468443fe04a23e
charlie <qianglin98@qq.com>
2022-12-08 20:21:31 +0800

    replace generate with McpResourceGenerator.Generate
    Signed-off-by: charlie <qianglin98@qq.com>

--- pkg/ingress/mcp/generator_test.go
+++ pkg/ingress/mcp/generator_test.go
@@ -3,56 +3,104 @@ package mcp
 import (
 	"testing"
 
+	"istio.io/istio/pilot/pkg/model"
+
 	extensions "istio.io/api/extensions/v1alpha1"
 	networking "istio.io/api/networking/v1alpha3"
 	"istio.io/istio/pkg/config"
 )
 
-func Test_generate(t *testing.T) {
+func TestGenerate(t *testing.T) {
 	tests := []struct {
-		Name  string
-		Spec  config.Spec
-		IsErr bool
+		name      string
+		fn        func() *model.PushContext
+		generator model.McpResourceGenerator
+		isErr     bool
 	}{
 		{
-			Name:  "VirtualService",
-			Spec:  &networking.VirtualService{},
-			IsErr: false,
+			name: "VirtualService",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &networking.VirtualService{},
+				}
+				ctx.AllVirtualServices = []config.Config{cfg}
+				return ctx
+			},
+			generator: VirtualServiceGenerator{},
+			isErr:     false,
 		},
 		{
-			Name:  "Gateway",
-			Spec:  &networking.Gateway{},
-			IsErr: false,
+			name: "Gateway",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &networking.Gateway{},
+				}
+				ctx.AllGateways = []config.Config{cfg}
+				return ctx
+			},
+			generator: GatewayGenerator{},
+			isErr:     false,
 		},
 		{
-			Name:  "EnvoyFilter",
-			Spec:  &networking.EnvoyFilter{},
-			IsErr: false,
+			name: "EnvoyFilter",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &networking.EnvoyFilter{},
+				}
+				ctx.AllEnvoyFilters = []config.Config{cfg}
+				return ctx
+			},
+			generator: EnvoyFilterGenerator{},
+			isErr:     false,
 		},
 		{
-			Name:  "DestinationRule",
-			Spec:  &networking.DestinationRule{},
-			IsErr: false,
+			name: "DestinationRule",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &networking.DestinationRule{},
+				}
+				ctx.AllDestinationRules = []config.Config{cfg}
+				return ctx
+			},
+			generator: DestinationRuleGenerator{},
+			isErr:     false,
 		},
 		{
-			Name:  "WasmPlugin",
-			Spec:  extensions.WasmPlugin{},
-			IsErr: false,
+			name: "WasmPlugin",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &extensions.WasmPlugin{},
+				}
+				ctx.AllWasmplugins = []config.Config{cfg}
+				return ctx
+			},
+			generator: WasmpluginGenerator{},
+			isErr:     false,
 		},
 		{
-			Name:  "string",
-			Spec:  "test",
-			IsErr: true,
+			name: "WasmPlugin with wrong config",
+			fn: func() *model.PushContext {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: "string",
+				}
+				ctx.AllWasmplugins = []config.Config{cfg}
+				return ctx
+			},
+			generator: WasmpluginGenerator{},
+			isErr:     true,
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			cfg := config.Config{
-				Spec: test.Spec,
-			}
-			_, _, err := generate(nil, []config.Config{cfg}, nil, nil)
-			if (err != nil && !test.IsErr) || (err == nil && test.IsErr) {
+		t.Run(test.name, func(t *testing.T) {
+			_, _, err := test.generator.Generate(nil, test.fn(), nil, nil)
+			if (err != nil && !test.isErr) || (err == nil && test.isErr) {
 				t.Errorf("Failed to generate config: %v", err)
 			}
 		})
