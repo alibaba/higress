b3c4fbf43eb1f5906723626edd64832cf685bb3d
charlie <qianglin98@qq.com>
2022-12-20 14:41:21 +0800

    add licence & fix the nil point
    Signed-off-by: charlie <qianglin98@qq.com>

--- pkg/bootstrap/server_test.go
+++ pkg/bootstrap/server_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2022 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package bootstrap
 
 import (
@@ -7,7 +21,8 @@ import (
 	"github.com/agiledragon/gomonkey/v2"
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pkg/keepalive"
-	kubelib "istio.io/istio/pkg/kube"
+
+	higresskube "github.com/alibaba/higress/pkg/kube"
 )
 
 func TestStartWithNoError(t *testing.T) {
@@ -17,7 +32,7 @@ func TestStartWithNoError(t *testing.T) {
 	)
 
 	mockFn := func(s *Server) error {
-		s.kubeClient = kubelib.NewFakeClient()
+		s.kubeClient = higresskube.NewFakeClient()
 		return nil
 	}
 
