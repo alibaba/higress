16da31e553a956db9ae149da81db86da2a2489b6
charlie <qianglin98@qq.com>
2023-02-01 16:40:07 +0800

    opt: optimize redundant code (#103)
    Signed-off-by: charlie <qianglin98@qq.com>

--- pkg/ingress/mcp/generator_test.go
+++ pkg/ingress/mcp/generator_test.go
@@ -102,6 +102,19 @@ func TestGenerate(t *testing.T) {
 			isErr:     false,
 		},
 		{
+			name: "ServiceEntry",
+			fn: func() (*model.PushContext, any) {
+				ctx := model.NewPushContext()
+				cfg := config.Config{
+					Spec: &networking.ServiceEntry{},
+				}
+				ctx.AllServiceEntries = []config.Config{cfg}
+				return ctx, cfg.Spec
+			},
+			generator: ServiceEntryGenerator{},
+			isErr:     false,
+		},
+		{
 			name: "WasmPlugin with wrong config",
 			fn: func() (*model.PushContext, any) {
 				ctx := model.NewPushContext()
