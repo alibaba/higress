8c0a3c67b3f2f16835ad2b36b35fb932f4329080
rinfx <893383980@qq.com>
2023-06-28 13:58:49 +0800

    update build-wasm-plugins.sh

--- tools/hack/build-wasm-plugins.sh
+++ tools/hack/build-wasm-plugins.sh
@@ -24,6 +24,9 @@ if [ ! -n "$INNER_PLUGIN_NAME" ]; then
     echo "build all wasmplugins under folder of $EXTENSIONS_DIR"
     for file in `ls $EXTENSIONS_DIR`                                   
         do
+            if [ file == 'waf' ]; then 
+                continue
+            fi
             if [ -d $EXTENSIONS_DIR$file ]; then 
                 name=${file##*/}
                 echo "build wasmplugin name of $name"
