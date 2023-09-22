b9aba7fb53a6422aa20b73dec220cb0f881cc30c
CH3CHO <ch3cho@qq.com>
2023-09-22 22:26:25 +0800

    Move submodules to the external folder

--- tools/hack/prebuild.sh
+++ tools/hack/prebuild.sh
@@ -1,46 +1,4 @@
 #!/bin/bash
 set -e
 
-ENVOY_VERSION="${ENVOY_VERSION:=1.26}"
-ISITO_VERSION="${ISTIO_VERSION:=1.18}"
-WORK_DIR=`cd $(dirname "$0")/../..;pwd`
-
-cd $WORK_DIR
-
-mkdir -p external/package
-
-envoy_repos=("go-control-plane" "envoy")
-
-for repo in ${envoy_repos[@]}; do
-    if [ -e external/$repo ];then
-        continue
-    fi
-    cp -r envoy/${ENVOY_VERSION}/$repo  external/$repo
-    for patch in `ls envoy/${ENVOY_VERSION}/patches/$repo/*.patch`; do
-        patch -d external/$repo -p1 < $patch
-    done
-    cd external/$repo
-    echo "gitdir: /parent/.git/modules/envoy/${ENVOY_VERSION}/$repo" > .git
-    if [ -f "go.mod" ]; then
-        go mod tidy
-    fi
-    cd $WORK_DIR
-done
-
-istio_repos=("api" "client-go" "pkg" "istio" "proxy")
-
-for repo in ${istio_repos[@]}; do
-    if [ -e external/$repo ];then
-        continue
-    fi
-    cp -r istio/${ISTIO_VERSION}/$repo  external/$repo
-    for patch in `ls istio/${ISTIO_VERSION}/patches/$repo/*.patch`; do
-        patch -d external/$repo -p1 < $patch
-    done
-    cd external/$repo
-    echo "gitdir: /parent/.git/modules/istio/${ISTIO_VERSION}/$repo" > .git
-    if [ -f "go.mod" ]; then
-        go mod tidy
-    fi
-    cd $WORK_DIR
-done
+# Nothing to do here.
