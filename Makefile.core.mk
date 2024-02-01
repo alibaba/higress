8b40188897d67456e8e0bbd4ad0a1f0efdda72a8
澄潭 <zty98751@alibaba-inc.com>
2024-02-01 19:59:03 +0800

    Update Makefile.core.mk

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -177,8 +177,8 @@ install: pre-install
 	cd helm/higress; helm dependency build
 	helm install higress helm/higress -n higress-system --create-namespace --set 'global.local=true'
 
-ENVOY_LATEST_IMAGE_TAG ?= sha-87c39d3
-ISTIO_LATEST_IMAGE_TAG ?= sha-87c39d3
+ENVOY_LATEST_IMAGE_TAG ?= sha-73d5cc3
+ISTIO_LATEST_IMAGE_TAG ?= sha-73d5cc3
 
 install-dev: pre-install
 	helm install higress helm/core -n higress-system --create-namespace --set 'controller.tag=$(TAG)' --set 'gateway.replicas=1' --set 'pilot.tag=$(ISTIO_LATEST_IMAGE_TAG)' --set 'gateway.tag=$(ENVOY_LATEST_IMAGE_TAG)' --set 'global.local=true'
