d797ebfbf8254d50bd6da184ac062eaa218e8437
LeBW <27091925@qq.com>
2024-03-06 11:45:52 +0800

    支持 epoch、max; 支持 suffix 选填

--- plugins/wasm-go/extensions/cache-control/main.go
+++ plugins/wasm-go/extensions/cache-control/main.go
@@ -21,18 +21,20 @@ func main() {
 }
 
 type CacheControlConfig struct {
-	suffix []string
-	maxAge int64
+	suffix  []string
+	expires string
 }
 
 func parseConfig(json gjson.Result, config *CacheControlConfig, log wrapper.Log) error {
 	suffix := json.Get("suffix").String()
-	parts := strings.Split(suffix, "|")
-	config.suffix = parts
+	if suffix != "" {
+		parts := strings.Split(suffix, "|")
+		config.suffix = parts
+	}
 
-	config.maxAge = json.Get("maxAge").Int()
+	config.expires = json.Get("expires").String()
 
-	log.Infof("suffix: %q, maxAge: %v", config.suffix, config.maxAge)
+	log.Infof("suffix: %q, expires: %s", config.suffix, config.expires)
 	return nil
 }
 
@@ -48,26 +50,36 @@ func onHttpRequestHeaders(ctx wrapper.HttpContext, config CacheControlConfig, lo
 }
 
 func onHttpResponseHeaders(ctx wrapper.HttpContext, config CacheControlConfig, log wrapper.Log) types.Action {
-	if len(config.suffix) > 0 {
+	hit := false
+	if len(config.suffix) == 0 {
+		hit = true
+	} else {
 		path, ok := ctx.GetContext("path").(string)
 		if !ok {
 			log.Error("failed to get request path")
 			return types.ActionContinue
 		}
 
-		hit := false
 		for _, part := range config.suffix {
 			if strings.HasSuffix(path, "."+part) {
 				hit = true
 				break
 			}
 		}
-
-		if hit {
+	}
+	if hit {
+		if config.expires == "max" {
+			proxywasm.AddHttpResponseHeader("Expires", "Thu, 31 Dec 2037 23:55:55 GMT")
+			proxywasm.AddHttpResponseHeader("Cache-Control", "maxAge=315360000")
+		} else if config.expires == "epoch" {
+			proxywasm.AddHttpResponseHeader("Expires", "Thu, 01 Jan 1970 00:00:01 GMT")
+			proxywasm.AddHttpResponseHeader("Cache-Control", "no-cache")
+		} else {
+			maxAge, _ := strconv.ParseInt(config.expires, 10, 64)
 			currentTime := time.Now()
-			expireTime := currentTime.Add(time.Duration(config.maxAge) * time.Second)
+			expireTime := currentTime.Add(time.Duration(maxAge) * time.Second)
 			proxywasm.AddHttpResponseHeader("Expires", expireTime.UTC().Format(http.TimeFormat))
-			proxywasm.AddHttpResponseHeader("Cache-Control", "maxAge="+strconv.FormatInt(config.maxAge, 10))
+			proxywasm.AddHttpResponseHeader("Cache-Control", "maxAge="+strconv.FormatInt(maxAge, 10))
 		}
 	}
 	return types.ActionContinue
