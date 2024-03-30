3b7632b5b05123a26395ff84fab4c8ec174870c9
haifzhu <haihaifeng@126.com>
2024-03-30 19:48:07 +0800

    add unit test cases to try-paths plugin

--- plugins/wasm-go/extensions/try-paths/go.mod
+++ plugins/wasm-go/extensions/try-paths/go.mod
@@ -5,14 +5,18 @@ go 1.19
 require (
 	github.com/alibaba/higress/plugins/wasm-go v1.3.5
 	github.com/higress-group/proxy-wasm-go-sdk v0.0.0-20240226065437-8f7a0b3c9071
+	github.com/stretchr/testify v1.8.4
 	github.com/tidwall/gjson v1.17.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/higress-group/nottinygc v0.0.0-20231101025119-e93c4c2f8520 // indirect
 	github.com/magefile/mage v1.14.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/tidwall/resp v0.1.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- plugins/wasm-go/extensions/try-paths/main.go
+++ plugins/wasm-go/extensions/try-paths/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 	"strings"
 
@@ -31,49 +30,6 @@ type TryPathsConfig struct {
 	client   wrapper.HttpClient
 }
 
-func Client(json gjson.Result) (wrapper.HttpClient, error) {
-	serviceSource := json.Get("serviceSource").String()
-	serviceName := json.Get("serviceName").String()
-	host := json.Get("host").String()
-	servicePort := json.Get("servicePort").Int()
-	if serviceName == "" || servicePort == 0 {
-		return nil, errors.New("invalid service config")
-	}
-	switch serviceSource {
-	case "k8s":
-		namespace := json.Get("namespace").String()
-		return wrapper.NewClusterClient(wrapper.K8sCluster{
-			ServiceName: serviceName,
-			Namespace:   namespace,
-			Port:        servicePort,
-			Host:        host,
-		}), nil
-	case "nacos":
-		namespace := json.Get("namespace").String()
-		return wrapper.NewClusterClient(wrapper.NacosCluster{
-			ServiceName: serviceName,
-			NamespaceID: namespace,
-			Port:        servicePort,
-			Host:        host,
-		}), nil
-	case "ip":
-		return wrapper.NewClusterClient(wrapper.StaticIpCluster{
-			ServiceName: serviceName,
-			Port:        servicePort,
-			Host:        host,
-		}), nil
-	case "dns":
-		domain := json.Get("domain").String()
-		return wrapper.NewClusterClient(wrapper.DnsCluster{
-			ServiceName: serviceName,
-			Port:        servicePort,
-			Domain:      domain,
-		}), nil
-	default:
-		return nil, errors.New("unknown service source: " + serviceSource)
-	}
-}
-
 func parseConfig(json gjson.Result, config *TryPathsConfig, log wrapper.Log) error {
 	// 解析出配置，更新到config中
 	for _, result := range json.Get("tryPaths").Array() {
@@ -96,26 +52,6 @@ func parseConfig(json gjson.Result, config *TryPathsConfig, log wrapper.Log) err
 	return nil
 }
 
-func convHttpHeadersToStruct(responseHeaders http.Header) [][2]string {
-	headerStruct := make([][2]string, len(responseHeaders))
-	i := 0
-	for key, values := range responseHeaders {
-		headerStruct[i][0] = key
-		headerStruct[i][1] = values[0]
-		i++
-	}
-	return headerStruct
-}
-
-func included(array []int, value int) bool {
-	for _, v := range array {
-		if v == value {
-			return true
-		}
-	}
-	return false
-}
-
 func tryHttpCall(ctx wrapper.HttpContext, config TryPathsConfig, index int, path string, log wrapper.Log) {
 	if len(config.tryPaths) == index {
 		proxywasm.ResumeHttpRequest()
@@ -124,8 +60,8 @@ func tryHttpCall(ctx wrapper.HttpContext, config TryPathsConfig, index int, path
 	requestPath := strings.Replace(config.tryPaths[index], VariableReplacedStr, path, -1)
 	config.client.Get(requestPath, nil,
 		func(statusCode int, responseHeaders http.Header, responseBody []byte) {
-			if !included(config.code, statusCode) {
-				proxywasm.SendHttpResponse(uint32(statusCode), convHttpHeadersToStruct(responseHeaders), responseBody, -1)
+			if !contains(config.code, statusCode) {
+				proxywasm.SendHttpResponse(uint32(statusCode), convertHttpHeadersToStruct(responseHeaders), responseBody, -1)
 				return
 			}
 			tryHttpCall(ctx, config, index+1, path, log)
