a69955f6730eeb5bee8ba9cafe07373bccce507d
jun <2456868764@qq.com>
2024-04-23 11:31:00 +0800

    add getting secertName from configmap if there is no matching found

--- pkg/ingress/kube/ingress/controller.go
+++ pkg/ingress/kube/ingress/controller.go
@@ -17,6 +17,7 @@ package ingress
 import (
 	"errors"
 	"fmt"
+	"github.com/alibaba/higress/pkg/cert"
 	"path"
 	"reflect"
 	"sort"
@@ -85,6 +86,8 @@ type controller struct {
 	secretController secret.SecretController
 
 	statusSyncer *statusSyncer
+
+	configMgr *cert.ConfigMgr
 }
 
 // NewController creates a new Kubernetes controller
@@ -103,6 +106,7 @@ func NewController(localKubeClient, client kubeclient.Client, options common.Opt
 		IngressLog.Infof("Skipping IngressClass, resource not supported for cluster %s", options.ClusterId)
 	}
 
+	configMgr, _ := cert.NewConfigMgr(options.SystemNamespace, client.Kube())
 	c := &controller{
 		options:          options,
 		queue:            q,
@@ -113,6 +117,7 @@ func NewController(localKubeClient, client kubeclient.Client, options common.Opt
 		serviceInformer:  serviceInformer.Informer(),
 		serviceLister:    serviceInformer.Lister(),
 		secretController: secretController,
+		configMgr:        configMgr,
 	}
 
 	handler := controllers.LatestVersionHandlerFuncs(controllers.EnqueueForSelf(q))
@@ -371,7 +376,7 @@ func (c *controller) ConvertGateway(convertOptions *common.ConvertOptions, wrapp
 		common.IncrementInvalidIngress(c.options.ClusterId, common.EmptyRule)
 		return fmt.Errorf("invalid ingress rule %s:%s in cluster %s, either `defaultBackend` or `rules` must be specified", cfg.Namespace, cfg.Name, c.options.ClusterId)
 	}
-
+	httpsCredentialConfig, _ := c.configMgr.GetConfigFromConfigmap()
 	for _, rule := range ingressV1Beta.Rules {
 		// Need create builder for every rule.
 		domainBuilder := &common.IngressDomainBuilder{
@@ -422,6 +427,10 @@ func (c *controller) ConvertGateway(convertOptions *common.ConvertOptions, wrapp
 
 		// Get tls secret matching the rule host
 		secretName := extractTLSSecretName(rule.Host, ingressV1Beta.TLS)
+		// If there is no matching secret, try to get it from configmap.
+		if secretName == "" && httpsCredentialConfig != nil {
+			secretName = httpsCredentialConfig.MatchSecretNameByDomain(rule.Host)
+		}
 		if secretName == "" {
 			// There no matching secret, so just skip.
 			continue
