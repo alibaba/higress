51b9d9ec4bc50192c33536f8a4eb07d5fa81c973
Yang Beining <35399433+Suchun-sv@users.noreply.github.com>
2024-05-28 11:29:36 +0800

    feat: Add the ZhipuAI (ChatGLM) provider to the ai-proxy wasm plugin #950 (#1007)
    Co-authored-by: Kent Dong <ch3cho@qq.com>

--- plugins/wasm-go/extensions/ai-proxy/provider/provider.go
+++ plugins/wasm-go/extensions/ai-proxy/provider/provider.go
@@ -23,6 +23,7 @@ const (
 	providerTypeBaichuan = "baichuan"
 	providerTypeYi       = "yi"
 	providerTypeDeepSeek = "deepseek"
+	providerTypeZhipuAi  = "zhipuai"
 	providerTypeOllama   = "ollama"
 
 	protocolOpenAI   = "openai"
@@ -61,6 +62,7 @@ var (
 		providerTypeBaichuan: &baichuanProviderInitializer{},
 		providerTypeYi:       &yiProviderInitializer{},
 		providerTypeDeepSeek: &deepseekProviderInitializer{},
+		providerTypeZhipuAi:  &zhipuAiProviderInitializer{},
 		providerTypeOllama:   &ollamaProviderInitializer{},
 	}
 )
@@ -91,7 +93,7 @@ type ResponseBodyHandler interface {
 
 type ProviderConfig struct {
 	// @Title zh-CN AI服务提供商
-	// @Description zh-CN AI服务提供商类型，目前支持的取值为："moonshot"、"qwen"、"openai"、"azure"、"baichuan"、"yi"、"ollama"
+	// @Description zh-CN AI服务提供商类型，目前支持的取值为："moonshot"、"qwen"、"openai"、"azure"、"baichuan"、"yi"、"zhipuai"、"ollama"
 	typ string `required:"true" yaml:"type" json:"type"`
 	// @Title zh-CN API Tokens
 	// @Description zh-CN 在请求AI服务时用于认证的API Token列表。不同的AI服务提供商可能有不同的名称。部分供应商只支持配置一个API Token（如Azure OpenAI）。
@@ -180,6 +182,7 @@ func (c *ProviderConfig) Validate() error {
 
 	if c.typ == "" {
 		return errors.New("missing type in provider config")
+    
 	}
 	initializer, has := providerInitializers[c.typ]
 	if !has {
