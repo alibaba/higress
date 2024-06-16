f82dd72b76bf6a3f71c29e31d7e50f44ffa5fa64
rinfx <893383980@qq.com>
2024-06-16 22:46:23 +0800

    update

---
+++ plugins/wasm-go/extensions/ai-token-ratelimit/config.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/alibaba/higress/plugins/wasm-go/pkg/wrapper"
+	"github.com/tidwall/gjson"
+	"github.com/zmap/go-iptree/iptree"
+)
+
+// 限流规则类型
+type limitRuleType string
+
+const (
+	limitByHeaderType limitRuleType = "limitByHeader"
+	limitByParamType  limitRuleType = "limitByParam"
+	limitByPerIpType  limitRuleType = "limitByPerIp"
+
+	RemoteAddrSourceType = "remote-addr"
+	HeaderSourceType     = "header"
+
+	DefaultRejectedCode uint32 = 429
+	DefaultRejectedMsg  string = "Too many requests"
+
+	Second           int64 = 1
+	SecondsPerMinute       = 60 * Second
+	SecondsPerHour         = 60 * SecondsPerMinute
+	SecondsPerDay          = 24 * SecondsPerHour
+)
+
+type ClusterKeyRateLimitConfig struct {
+	ruleName             string        // 限流规则名称
+	limitType            limitRuleType // 限流类型
+	limitByHeader        string        // 根据http请求头限流
+	limitByParam         string        // 根据url参数限流
+	limitByPerIp         LimitByPerIp  // 根据对端ip限流
+	limitItems           []LimitItem   // 限流配置 key为限流的ip地址或者ip段
+	showLimitQuotaHeader bool          // 响应头中是否显示X-RateLimit-Limit和X-RateLimit-Remaining
+	rejectedCode         uint32        // 当请求超过阈值被拒绝时,返回的HTTP状态码
+	rejectedMsg          string        // 当请求超过阈值被拒绝时,返回的响应体
+	redisClient          wrapper.RedisClient
+}
+
+type LimitByPerIp struct {
+	sourceType string // ip来源类型
+	headerName string // 根据该请求头获取客户端ip
+}
+
+type LimitItem struct {
+	key        string         // 限流key
+	ipNet      *iptree.IPTree // 限流key转换的ip地址或者ip段
+	count      int64          // 指定时间窗口内的总请求数量阈值
+	timeWindow int64          // 时间窗口大小
+}
+
+func initRedisClusterClient(json gjson.Result, config *ClusterKeyRateLimitConfig) error {
+	redisConfig := json.Get("redis")
+	if !redisConfig.Exists() {
+		return errors.New("missing redis in config")
+	}
+	serviceName := redisConfig.Get("service_name").String()
+	if serviceName == "" {
+		return errors.New("redis service name must not be empty")
+	}
+	servicePort := int(redisConfig.Get("service_port").Int())
+	if servicePort == 0 {
+		if strings.HasSuffix(serviceName, ".static") {
+			// use default logic port which is 80 for static service
+			servicePort = 80
+		} else {
+			servicePort = 6379
+		}
+	}
+	username := redisConfig.Get("username").String()
+	password := redisConfig.Get("password").String()
+	timeout := int(redisConfig.Get("timeout").Int())
+	if timeout == 0 {
+		timeout = 1000
+	}
+	config.redisClient = wrapper.NewRedisClusterClient(wrapper.FQDNCluster{
+		FQDN: serviceName,
+		Port: int64(servicePort),
+	})
+	return config.redisClient.Init(username, password, int64(timeout))
+}
+
+func parseClusterKeyRateLimitConfig(json gjson.Result, config *ClusterKeyRateLimitConfig, log wrapper.Log) error {
+	ruleName := json.Get("rule_name")
+	if !ruleName.Exists() {
+		return errors.New("missing rule_name in config")
+	}
+	config.ruleName = ruleName.String()
+
+	// 根据配置区分限流类型
+	var limitType limitRuleType
+	limitByHeader := json.Get("limit_by_header")
+	if limitByHeader.Exists() && limitByHeader.String() != "" {
+		config.limitByHeader = limitByHeader.String()
+		limitType = limitByHeaderType
+	}
+
+	limitByParam := json.Get("limit_by_param")
+	if limitByParam.Exists() && limitByParam.String() != "" {
+		config.limitByParam = limitByParam.String()
+		limitType = limitByParamType
+	}
+
+	limitByPerIpResult := json.Get("limit_by_per_ip")
+	if limitByPerIpResult.Exists() && limitByPerIpResult.String() != "" {
+		limitByPerIp := limitByPerIpResult.String()
+		if strings.HasPrefix(limitByPerIp, "from-header-") {
+			headerName := limitByPerIp[len("from-header-"):]
+			if headerName == "" {
+				return errors.New("limit_by_per_ip parse error: empty after 'from-header-'")
+			}
+			config.limitByPerIp = LimitByPerIp{
+				sourceType: HeaderSourceType,
+				headerName: headerName,
+			}
+		} else if limitByPerIp == "from-remote-addr" {
+			config.limitByPerIp = LimitByPerIp{
+				sourceType: RemoteAddrSourceType,
+				headerName: "",
+			}
+		} else {
+			return errors.New("the 'limit_by_per_ip' restriction must start with 'from-header-' or be exactly 'from-remote-addr'")
+		}
+		limitType = limitByPerIpType
+	}
+	if limitType == "" {
+		return errors.New("only one of 'limit_by_header' and 'limit_by_param' and 'limit_by_per_ip' can be set")
+	}
+	config.limitType = limitType
+
+	// 初始化LimitItem
+	err := initLimitItems(json, config, log)
+	if err != nil {
+		return err
+	}
+
+	showLimitQuotaHeader := json.Get("show_limit_quota_header")
+	if showLimitQuotaHeader.Exists() {
+		config.showLimitQuotaHeader = showLimitQuotaHeader.Bool()
+	}
+
+	rejectedCode := json.Get("rejected_code")
+	if rejectedCode.Exists() {
+		config.rejectedCode = uint32(rejectedCode.Uint())
+	} else {
+		config.rejectedCode = DefaultRejectedCode
+	}
+	rejectedMsg := json.Get("rejected_msg")
+	if rejectedCode.Exists() {
+		config.rejectedMsg = rejectedMsg.String()
+	} else {
+		config.rejectedMsg = DefaultRejectedMsg
+	}
+	return nil
+}
+
+func initLimitItems(json gjson.Result, config *ClusterKeyRateLimitConfig, log wrapper.Log) error {
+	limitKeys := json.Get("limit_keys")
+	if !limitKeys.Exists() {
+		return errors.New("missing limit_keys in config")
+	}
+	if len(limitKeys.Array()) == 0 {
+		return errors.New("config limit_keys cannot be empty")
+	}
+	var limitItems []LimitItem
+	for _, item := range limitKeys.Array() {
+		key := item.Get("key")
+		if !key.Exists() || key.String() == "" {
+			return errors.New("limit_keys key is required")
+		}
+		var ipNet *iptree.IPTree
+		if config.limitType == limitByPerIpType {
+			var err error
+			ipNet, err = parseIPNet(key.String())
+			if err != nil {
+				log.Errorf("parseIPNet error: %v", err)
+				return err
+			}
+		} else {
+			ipNet = nil
+		}
+
+		qps := item.Get("query_per_second")
+		if qps.Exists() && qps.Int() > 0 {
+			limitItems = append(limitItems, LimitItem{
+				key:        key.String(),
+				ipNet:      ipNet,
+				count:      qps.Int(),
+				timeWindow: Second,
+			})
+			continue
+		}
+		qpm := item.Get("query_per_minute")
+		if qpm.Exists() && qpm.Int() > 0 {
+			limitItems = append(limitItems, LimitItem{
+				key:        key.String(),
+				ipNet:      ipNet,
+				count:      qpm.Int(),
+				timeWindow: SecondsPerMinute,
+			})
+			continue
+		}
+		qph := item.Get("query_per_hour")
+		if qph.Exists() && qph.Int() > 0 {
+			limitItems = append(limitItems, LimitItem{
+				key:        key.String(),
+				ipNet:      ipNet,
+				count:      qph.Int(),
+				timeWindow: SecondsPerHour,
+			})
+			continue
+		}
+		qpd := item.Get("query_per_day")
+		if qpd.Exists() && qpd.Int() > 0 {
+			limitItems = append(limitItems, LimitItem{
+				key:        key.String(),
+				ipNet:      ipNet,
+				count:      qpd.Int(),
+				timeWindow: SecondsPerDay,
+			})
+			continue
+		}
+	}
+	config.limitItems = limitItems
+	return nil
+}
--- plugins/wasm-go/extensions/ai-token-ratelimit/main.go
+++ plugins/wasm-go/extensions/ai-token-ratelimit/main.go
@@ -1,8 +1,25 @@
+// Copyright (c) 2024 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"errors"
+	"fmt"
+	"net"
+	"net/url"
 	"strconv"
+	"strings"
 
 	"github.com/alibaba/higress/plugins/wasm-go/pkg/wrapper"
 	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
@@ -13,85 +30,125 @@ import (
 
 func main() {
 	wrapper.SetCtx(
-		"ai-token-manage",
+		"ai-token-ratelimit",
 		wrapper.ParseConfigBy(parseConfig),
 		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+		wrapper.ProcessResponseHeadersBy(onHttpResponseHeaders),
 		wrapper.ProcessStreamingResponseBodyBy(onHttpStreamingBody),
 	)
 }
 
-type TokenManageConfig struct {
-	client  wrapper.RedisClient
-	tpm     int
-	metrics map[string]proxywasm.MetricCounter
-}
-
-func (config *TokenManageConfig) incrementCounter(metricName string, inc uint64) {
-	counter, ok := config.metrics[metricName]
-	if !ok {
-		counter = proxywasm.DefineCounterMetric(metricName)
-		config.metrics[metricName] = counter
-	}
-	counter.Increment(inc)
-}
-
 const (
-	redisKey = "higress-token-ratelimit"
+	ClusterRateLimitFormat string = "higress-token-ratelimit:%s:%s"
+	FixedWindowScript      string = `
+    	local ttl = redis.call('ttl', KEYS[1])
+    	if ttl < 0 then
+        	redis.call('set', KEYS[1], ARGV[1] - 1, 'EX', ARGV[2])
+        	return {ARGV[1], ARGV[1] - 1, ARGV[2]}
+    	end
+    	return {ARGV[1], redis.call('incrby', KEYS[1], -1), ttl}
+	`
 )
 
-func parseConfig(json gjson.Result, config *TokenManageConfig, log wrapper.Log) error {
-	config.metrics = make(map[string]proxywasm.MetricCounter)
-	config.tpm = int(json.Get("tpm").Int())
+const (
+	LimitContextKey string = "LimitContext" // 限流上下文信息
 
-	serviceSource := json.Get("serviceSource").String()
-	serviceName := json.Get("serviceName").String()
-	servicePort := json.Get("servicePort").Int()
-	username := json.Get("username").String()
-	password := json.Get("password").String()
-	timeout := json.Get("timeout").Int()
-	if serviceName == "" || servicePort == 0 {
-		return errors.New("invalid service config")
-	}
-	switch serviceSource {
-	case "ip":
-		config.client = wrapper.NewRedisClusterClient(wrapper.StaticIpCluster{
-			ServiceName: serviceName,
-			Port:        servicePort,
-		})
-	case "dns":
-		domain := json.Get("domain").String()
-		config.client = wrapper.NewRedisClusterClient(wrapper.DnsCluster{
-			ServiceName: serviceName,
-			Port:        servicePort,
-			Domain:      domain,
-		})
-	default:
-		return errors.New("unknown service source: " + serviceSource)
-	}
-	return config.client.Init(username, password, timeout)
+	RateLimitLimitHeader     string = "X-RateLimit-Limit"     // 限制的总请求数
+	RateLimitRemainingHeader string = "X-RateLimit-Remaining" // 剩余还可以发送的请求数
+	RateLimitResetHeader     string = "X-RateLimit-Reset"     // 限流重置时间（触发限流时返回）
+)
+
+type LimitContext struct {
+	count     int
+	remaining int
+	reset     int
 }
 
-func onHttpRequestHeaders(ctx wrapper.HttpContext, config TokenManageConfig, log wrapper.Log) types.Action {
-	config.incrementCounter("ai_token_ratelimit_request_total", 1)
-	err := config.client.Get(redisKey, func(response resp.Value) {
-		if response.Error() != nil {
-			log.Errorf("redisCall Get error: %v", response.Error())
-			proxywasm.ResumeHttpRequest()
+func parseConfig(json gjson.Result, config *ClusterKeyRateLimitConfig, log wrapper.Log) error {
+	err := initRedisClusterClient(json, config)
+	if err != nil {
+		return err
+	}
+	err = parseClusterKeyRateLimitConfig(json, config, log)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func onHttpRequestHeaders(ctx wrapper.HttpContext, config ClusterKeyRateLimitConfig, log wrapper.Log) types.Action {
+	// 判断是否命中限流规则
+	key, limitItem := hitRateLimitRule(ctx, config, log)
+	if limitItem == nil {
+		return types.ActionContinue
+	}
+
+	// 构建redis限流key和参数
+	limitKey := fmt.Sprintf(ClusterRateLimitFormat, config.ruleName, key)
+	keys := []interface{}{limitKey}
+	args := []interface{}{limitItem.count, limitItem.timeWindow}
+	// 执行限流逻辑
+	err := config.redisClient.Eval(FixedWindowScript, 1, keys, args, func(response resp.Value) {
+		defer func() {
+			_ = proxywasm.ResumeHttpRequest()
+		}()
+		resultArray := response.Array()
+		if len(resultArray) != 3 {
+			log.Errorf("redis response parse error, response: %v", response)
+			return
+		}
+		context := LimitContext{
+			count:     resultArray[0].Integer(),
+			remaining: resultArray[1].Integer(),
+			reset:     resultArray[2].Integer(),
+		}
+		if context.remaining < 0 {
+			// 触发限流
+			rejected(config, context)
 		} else {
-			if !response.IsNull() && response.Integer() > config.tpm {
-				config.incrementCounter("ai_token_ratelimit_request_deny", 1)
-				proxywasm.SendHttpResponse(429, nil, []byte("Too many requests\n"), -1)
-			} else {
-				proxywasm.ResumeHttpRequest()
-			}
+			ctx.SetContext(LimitContextKey, context)
 		}
 	})
 	if err != nil {
-		log.Errorf("Error occured while calling Get.")
+		log.Errorf("redis call failed: %v", err)
 		return types.ActionContinue
-	} else {
-		return types.ActionPause
 	}
+	return types.ActionPause
+}
+
+// {
+// 	config.incrementCounter("ai_token_ratelimit_request_total", 1)
+// 	err := config.client.Get(redisKey, func(response resp.Value) {
+// 		if response.Error() != nil {
+// 			log.Errorf("redisCall Get error: %v", response.Error())
+// 			proxywasm.ResumeHttpRequest()
+// 		} else {
+// 			if !response.IsNull() && response.Integer() > config.tpm {
+// 				config.incrementCounter("ai_token_ratelimit_request_deny", 1)
+// 				proxywasm.SendHttpResponse(429, nil, []byte("Too many requests\n"), -1)
+// 			} else {
+// 				proxywasm.ResumeHttpRequest()
+// 			}
+// 		}
+// 	})
+// 	if err != nil {
+// 		log.Errorf("Error occured while calling Get.")
+// 		return types.ActionContinue
+// 	} else {
+// 		return types.ActionPause
+// 	}
+// }
+
+func onHttpResponseHeaders(ctx wrapper.HttpContext, config ClusterKeyRateLimitConfig, log wrapper.Log) types.Action {
+	limitContext, ok := ctx.GetContext(LimitContextKey).(LimitContext)
+	if !ok {
+		return types.ActionContinue
+	}
+	if config.showLimitQuotaHeader {
+		_ = proxywasm.ReplaceHttpResponseHeader(RateLimitLimitHeader, strconv.Itoa(limitContext.count))
+		_ = proxywasm.ReplaceHttpResponseHeader(RateLimitRemainingHeader, strconv.Itoa(limitContext.remaining))
+	}
+	return types.ActionContinue
 }
 
 func onHttpStreamingBody(ctx wrapper.HttpContext, config TokenManageConfig, data []byte, endOfStream bool, log wrapper.Log) []byte {
@@ -130,4 +187,85 @@ func onHttpStreamingBody(ctx wrapper.HttpContext, config TokenManageConfig, data
 	} else {
 		return data
 	}
+}
+
+func hitRateLimitRule(ctx wrapper.HttpContext, config ClusterKeyRateLimitConfig, log wrapper.Log) (string, *LimitItem) {
+	switch config.limitType {
+	case limitByHeaderType:
+		headerVal, err := proxywasm.GetHttpRequestHeader(config.limitByHeader)
+		if err != nil {
+			log.Debugf("failed to get request header %s: %v", config.limitByHeader, err)
+			return "", nil
+		}
+		return headerVal, findMatchingItem(config.limitItems, headerVal)
+	case limitByParamType:
+		parse, _ := url.Parse(ctx.Path())
+		query, _ := url.ParseQuery(parse.RawQuery)
+		val, ok := query[config.limitByParam]
+		if !ok {
+			log.Debugf("request param %s is empty", config.limitByParam)
+			return "", nil
+		} else {
+			return val[0], findMatchingItem(config.limitItems, val[0])
+		}
+	case limitByPerIpType:
+		realIp, err := getDownStreamIp(config)
+		if err != nil {
+			log.Warnf("failed to get down stream ip: %v", err)
+			return "", nil
+		}
+		for _, item := range config.limitItems {
+			if _, found, _ := item.ipNet.Get(realIp); !found {
+				continue
+			}
+			return realIp.String(), &item
+		}
+	}
+	return "", nil
+}
+
+func findMatchingItem(items []LimitItem, key string) *LimitItem {
+	for _, item := range items {
+		if item.key == key {
+			return &item
+		}
+	}
+	return nil
+}
+
+func getDownStreamIp(config ClusterKeyRateLimitConfig) (net.IP, error) {
+	var (
+		realIpStr string
+		err       error
+	)
+	if config.limitByPerIp.sourceType == HeaderSourceType {
+		realIpStr, err = proxywasm.GetHttpRequestHeader(config.limitByPerIp.headerName)
+		if err == nil {
+			realIpStr = strings.Split(strings.Trim(realIpStr, " "), ",")[0]
+		}
+	} else {
+		var bs []byte
+		bs, err = proxywasm.GetProperty([]string{"source", "address"})
+		realIpStr = string(bs)
+	}
+	if err != nil {
+		return nil, err
+	}
+	ip := parseIP(realIpStr)
+	realIP := net.ParseIP(ip)
+	if realIP == nil {
+		return nil, fmt.Errorf("invalid ip[%s]", ip)
+	}
+	return realIP, nil
+}
+
+func rejected(config ClusterKeyRateLimitConfig, context LimitContext) {
+	headers := make(map[string][]string)
+	headers[RateLimitResetHeader] = []string{strconv.Itoa(context.reset)}
+	if config.showLimitQuotaHeader {
+		headers[RateLimitLimitHeader] = []string{strconv.Itoa(context.count)}
+		headers[RateLimitRemainingHeader] = []string{strconv.Itoa(0)}
+	}
+	_ = proxywasm.SendHttpResponse(
+		config.rejectedCode, reconvertHeaders(headers), []byte(config.rejectedMsg), -1)
 }
