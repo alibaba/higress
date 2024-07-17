8392670e5aff53cbe54fb7a6931140c5c596f01f
liurong.liu <liurong.liu@alibaba-inc.com>
2024-07-17 11:15:42 +0800

    add e2e test

--- test/e2e/conformance/tests/httproute-mirror-target-service.go
+++ test/e2e/conformance/tests/httproute-mirror-target-service.go
@@ -15,10 +15,19 @@
 package tests
 
 import (
+	"bytes"
+	"context"
+	"io"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/alibaba/higress/test/e2e/conformance/utils/http"
 	"github.com/alibaba/higress/test/e2e/conformance/utils/suite"
+	v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 func init() {
@@ -39,7 +48,7 @@ var HTTPRouteMirrorTargetService = suite.ConformanceTest{
 				},
 				Request: http.AssertionRequest{
 					ActualRequest: http.Request{
-						Path: "/foo",
+						Path: "/mirror",
 					},
 				},
 				Response: http.AssertionResponse{
@@ -53,6 +62,45 @@ var HTTPRouteMirrorTargetService = suite.ConformanceTest{
 		t.Run("HTTPRoute mirror request to target service", func(t *testing.T) {
 			for _, testcase := range testcases {
 				http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, suite.GatewayAddress, testcase)
+				//check mirror's logs for request
+				cfg, err := config.GetConfig()
+				if err != nil {
+					t.Fatalf("[httproute-mirror] get config failed.")
+					return
+				}
+				clientSet, err := kubernetes.NewForConfig(cfg)
+				if err != nil {
+					t.Fatalf("[httproute-mirror] init clientset failed.")
+					return
+				}
+				pods, err := clientSet.CoreV1().Pods("higress-conformance-infra").List(context.Background(), meta_v1.ListOptions{
+					LabelSelector: meta_v1.FormatLabelSelector(&meta_v1.LabelSelector{MatchLabels: map[string]string{"app": "infra-backend-mirror"}}),
+				})
+				if err != nil || len(pods.Items) == 0 {
+					t.Fatalf("[httproute-mirror] get pods by label of [\"app\": \"infra-backend-mirror\"] failed.")
+					return
+				}
+				req := clientSet.CoreV1().Pods("higress-conformance-infra").GetLogs(pods.Items[0].Name, &v1.PodLogOptions{
+					Container: "infra-backend-mirror",
+					SinceTime: &meta_v1.Time{Time: time.Now().Add(-time.Second * 10)},
+				})
+				podLogs, err := req.Stream(context.Background())
+				defer podLogs.Close()
+				if err != nil {
+					t.Fatalf("[httproute-mirror] init pod logs stream failed.")
+					return
+				}
+
+				podBuf := new(bytes.Buffer)
+				_, err = io.Copy(podBuf, podLogs)
+				if err != nil {
+					t.Fatalf("[httproute-mirror] read pod logs stream failed.")
+					return
+				}
+				if !strings.Contains(podBuf.String(), "Echoing back request made to /mirror") {
+					t.Fatalf("[httproute-mirror] mirror pod hasn't received any mirror requests in logs.")
+					return
+				}
 			}
 		})
 	},
