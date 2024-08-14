93207e8cd187c0aaceeff09e89902da55e114d6b
johnlanni <zty98751@alibaba-inc.com>
2024-08-14 15:45:33 +0800

    update client code

--- client/pkg/clientset/versioned/typed/extensions/v1alpha1/wasmplugin.gen.go
+++ client/pkg/clientset/versioned/typed/extensions/v1alpha1/wasmplugin.gen.go
@@ -18,9 +18,12 @@ package v1alpha1
 
 import (
 	"context"
+	json "encoding/json"
+	"fmt"
 	"time"
 
 	v1alpha1 "github.com/alibaba/higress/client/pkg/apis/extensions/v1alpha1"
+	extensionsv1alpha1 "github.com/alibaba/higress/client/pkg/applyconfiguration/extensions/v1alpha1"
 	scheme "github.com/alibaba/higress/client/pkg/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
@@ -45,6 +48,8 @@ type WasmPluginInterface interface {
 	List(ctx context.Context, opts v1.ListOptions) (*v1alpha1.WasmPluginList, error)
 	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
 	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.WasmPlugin, err error)
+	Apply(ctx context.Context, wasmPlugin *extensionsv1alpha1.WasmPluginApplyConfiguration, opts v1.ApplyOptions) (result *v1alpha1.WasmPlugin, err error)
+	ApplyStatus(ctx context.Context, wasmPlugin *extensionsv1alpha1.WasmPluginApplyConfiguration, opts v1.ApplyOptions) (result *v1alpha1.WasmPlugin, err error)
 	WasmPluginExpansion
 }
 
@@ -186,6 +191,62 @@ func (c *wasmPlugins) Patch(ctx context.Context, name string, pt types.PatchType
 		Name(name).
 		SubResource(subresources...).
 		VersionedParams(&opts, scheme.ParameterCodec).
+		Body(data).
+		Do(ctx).
+		Into(result)
+	return
+}
+
+// Apply takes the given apply declarative configuration, applies it and returns the applied wasmPlugin.
+func (c *wasmPlugins) Apply(ctx context.Context, wasmPlugin *extensionsv1alpha1.WasmPluginApplyConfiguration, opts v1.ApplyOptions) (result *v1alpha1.WasmPlugin, err error) {
+	if wasmPlugin == nil {
+		return nil, fmt.Errorf("wasmPlugin provided to Apply must not be nil")
+	}
+	patchOpts := opts.ToPatchOptions()
+	data, err := json.Marshal(wasmPlugin)
+	if err != nil {
+		return nil, err
+	}
+	name := wasmPlugin.Name
+	if name == nil {
+		return nil, fmt.Errorf("wasmPlugin.Name must be provided to Apply")
+	}
+	result = &v1alpha1.WasmPlugin{}
+	err = c.client.Patch(types.ApplyPatchType).
+		Namespace(c.ns).
+		Resource("wasmplugins").
+		Name(*name).
+		VersionedParams(&patchOpts, scheme.ParameterCodec).
+		Body(data).
+		Do(ctx).
+		Into(result)
+	return
+}
+
+// ApplyStatus was generated because the type contains a Status member.
+// Add a +genclient:noStatus comment above the type to avoid generating ApplyStatus().
+func (c *wasmPlugins) ApplyStatus(ctx context.Context, wasmPlugin *extensionsv1alpha1.WasmPluginApplyConfiguration, opts v1.ApplyOptions) (result *v1alpha1.WasmPlugin, err error) {
+	if wasmPlugin == nil {
+		return nil, fmt.Errorf("wasmPlugin provided to Apply must not be nil")
+	}
+	patchOpts := opts.ToPatchOptions()
+	data, err := json.Marshal(wasmPlugin)
+	if err != nil {
+		return nil, err
+	}
+
+	name := wasmPlugin.Name
+	if name == nil {
+		return nil, fmt.Errorf("wasmPlugin.Name must be provided to Apply")
+	}
+
+	result = &v1alpha1.WasmPlugin{}
+	err = c.client.Patch(types.ApplyPatchType).
+		Namespace(c.ns).
+		Resource("wasmplugins").
+		Name(*name).
+		SubResource("status").
+		VersionedParams(&patchOpts, scheme.ParameterCodec).
 		Body(data).
 		Do(ctx).
 		Into(result)
