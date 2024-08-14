ba98f3a7ad9d69b553429b91de4d6d73ff6c8ffd
mamba <371510756@qq.com>
2024-08-14 15:41:32 +0800

    feat: 🎸 frontend-gray plugin support cdn type deploy (#1178)
    Co-authored-by: Kent Dong <ch3cho@qq.com>

--- plugins/wasm-go/extensions/frontend-gray/main.go
+++ plugins/wasm-go/extensions/frontend-gray/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/alibaba/higress/plugins/wasm-go/extensions/frontend-gray/config"
 	"github.com/alibaba/higress/plugins/wasm-go/extensions/frontend-gray/util"
 
@@ -15,6 +19,9 @@ func main() {
 		"frontend-gray",
 		wrapper.ParseConfigBy(parseConfig),
 		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+		wrapper.ProcessResponseHeadersBy(onHttpResponseHeader),
+		wrapper.ProcessResponseBodyBy(onHttpResponseBody),
+		wrapper.ProcessStreamingResponseBodyBy(onStreamingResponseBody),
 	)
 }
 
@@ -24,55 +31,146 @@ func parseConfig(json gjson.Result, grayConfig *config.GrayConfig, log wrapper.L
 	return nil
 }
 
-// FilterGrayRule 过滤灰度规则
-func FilterGrayRule(grayConfig *config.GrayConfig, grayKeyValue string, log wrapper.Log) *config.GrayDeployments {
-	for _, grayDeployment := range grayConfig.GrayDeployments {
-		if !grayDeployment.Enabled {
-			// 跳过Enabled=false
-			continue
-		}
-		grayRule := util.GetRule(grayConfig.Rules, grayDeployment.Name)
-		// 首先：先校验用户名单ID
-		if grayRule.GrayKeyValue != nil && len(grayRule.GrayKeyValue) > 0 && grayKeyValue != "" {
-			if util.Contains(grayRule.GrayKeyValue, grayKeyValue) {
-				log.Infof("x-mse-tag: %s, grayKeyValue: %s", grayDeployment.Version, grayKeyValue)
-				return grayDeployment
-			}
-		}
-		//	第二：校验Cookie中的 GrayTagKey
-		if grayRule.GrayTagKey != "" && grayRule.GrayTagValue != nil && len(grayRule.GrayTagValue) > 0 {
-			cookieStr, _ := proxywasm.GetHttpRequestHeader("cookie")
-			grayTagValue := util.GetValueByCookie(cookieStr, grayRule.GrayTagKey)
-			if util.Contains(grayRule.GrayTagValue, grayTagValue) {
-				log.Infof("x-mse-tag: %s, grayTag: %s=%s", grayDeployment.Version, grayRule.GrayTagKey, grayTagValue)
-				return grayDeployment
-			}
-		}
+func onHttpRequestHeaders(ctx wrapper.HttpContext, grayConfig config.GrayConfig, log wrapper.Log) types.Action {
+	if !util.IsGrayEnabled(grayConfig) {
+		return types.ActionContinue
 	}
-	log.Infof("x-mse-tag: %s, grayKeyValue: %s", grayConfig.BaseDeployment.Version, grayKeyValue)
-	return nil
+
+	cookies, _ := proxywasm.GetHttpRequestHeader("cookie")
+	path, _ := proxywasm.GetHttpRequestHeader(":path")
+	fetchMode, _ := proxywasm.GetHttpRequestHeader("sec-fetch-mode")
+
+	isIndex := util.IsIndexRequest(fetchMode, path)
+	hasRewrite := len(grayConfig.Rewrite.File) > 0 || len(grayConfig.Rewrite.Index) > 0
+	grayKeyValue := util.GetGrayKey(util.ExtractCookieValueByKey(cookies, grayConfig.GrayKey), grayConfig.GraySubKey)
+
+	// 如果有重写的配置，则进行重写
+	if hasRewrite {
+		// 禁止重新路由，要在更改Header之前操作，否则会失效
+		ctx.DisableReroute()
+	}
+
+	// 删除Accept-Encoding，避免压缩， 如果是压缩的内容，后续插件就没法处理了
+	_ = proxywasm.RemoveHttpRequestHeader("Accept-Encoding")
+	_ = proxywasm.RemoveHttpRequestHeader("Content-Length")
+
+	grayDeployment := util.FilterGrayRule(&grayConfig, grayKeyValue, log.Infof)
+	frontendVersion := util.GetVersion(grayConfig.BaseDeployment.Version, cookies, isIndex)
+	backendVersion := ""
+
+	// 命中灰度规则
+	if grayDeployment != nil {
+		frontendVersion = util.GetVersion(grayDeployment.Version, cookies, isIndex)
+		backendVersion = grayDeployment.BackendVersion
+	}
+
+	proxywasm.AddHttpRequestHeader(config.XHigressTag, frontendVersion)
+
+	ctx.SetContext(config.XPreHigressTag, frontendVersion)
+	ctx.SetContext(config.XMseTag, backendVersion)
+	ctx.SetContext(config.IsIndex, isIndex)
+
+	rewrite := grayConfig.Rewrite
+	if rewrite.Host != "" {
+		proxywasm.ReplaceHttpRequestHeader("HOST", rewrite.Host)
+	}
+
+	if hasRewrite {
+		rewritePath := path
+		if isIndex {
+			rewritePath = util.IndexRewrite(path, frontendVersion, grayConfig.Rewrite.Index)
+		} else {
+			rewritePath = util.PrefixFileRewrite(path, frontendVersion, grayConfig.Rewrite.File)
+		}
+		log.Infof("rewrite path: %s %s %v", path, frontendVersion, rewritePath)
+		proxywasm.ReplaceHttpRequestHeader(":path", rewritePath)
+	}
+
+	return types.ActionContinue
 }
 
-func onHttpRequestHeaders(ctx wrapper.HttpContext, grayConfig config.GrayConfig, log wrapper.Log) types.Action {
-	// 优先从cookie中获取，如果拿不到再从header中获取
-	cookieStr, _ := proxywasm.GetHttpRequestHeader("cookie")
-	grayHeaderKey, _ := proxywasm.GetHttpRequestHeader(grayConfig.GrayKey)
-	grayKeyValue := util.GetValueByCookie(cookieStr, grayConfig.GrayKey)
-	proxywasm.RemoveHttpRequestHeader("Accept-Encoding")
-	// 优先从Cookie中获取，否则从header中获取
-	if grayKeyValue == "" {
-		grayKeyValue = grayHeaderKey
+func onHttpResponseHeader(ctx wrapper.HttpContext, grayConfig config.GrayConfig, log wrapper.Log) types.Action {
+	if !util.IsGrayEnabled(grayConfig) {
+		return types.ActionContinue
 	}
-	// 如果有子key, 尝试从子key中获取值
-	if grayConfig.GraySubKey != "" {
-		subKeyValue := util.GetBySubKey(grayKeyValue, grayConfig.GraySubKey)
-		if subKeyValue != "" {
-			grayKeyValue = subKeyValue
+	status, err := proxywasm.GetHttpResponseHeader(":status")
+	contentType, _ := proxywasm.GetHttpResponseHeader("Content-Type")
+	if err != nil || status != "200" {
+		isIndex := ctx.GetContext(config.IsIndex)
+		if status == "404" {
+			if grayConfig.Rewrite.NotFound != "" && isIndex != nil && isIndex.(bool) {
+				ctx.SetContext(config.NotFound, true)
+				responseHeaders, _ := proxywasm.GetHttpResponseHeaders()
+				headersMap := util.ConvertHeaders(responseHeaders)
+				headersMap[":status"][0] = "200"
+				headersMap["content-type"][0] = "text/html"
+				delete(headersMap, "content-length")
+				proxywasm.ReplaceHttpResponseHeaders(util.ReconvertHeaders(headersMap))
+				ctx.BufferResponseBody()
+				return types.ActionContinue
+			} else {
+				ctx.DontReadResponseBody()
+			}
 		}
+		log.Errorf("error status: %s, error message: %v", status, err)
+		return types.ActionContinue
 	}
-	grayDeployment := FilterGrayRule(&grayConfig, grayKeyValue, log)
-	if grayDeployment != nil {
-		proxywasm.AddHttpRequestHeader("x-mse-tag", grayDeployment.Version)
+
+	// 删除content-length，可能要修改Response返回值
+	proxywasm.RemoveHttpResponseHeader("Content-Length")
+
+	// 删除Content-Disposition，避免自动下载文件
+	proxywasm.RemoveHttpResponseHeader("Content-Disposition")
+
+	if strings.HasPrefix(contentType, "text/html") {
+		ctx.SetContext(config.IsHTML, true)
+		// 不会进去Streaming 的Body处理
+		ctx.BufferResponseBody()
+
+		// 添加Cache-Control 头部，禁止缓存
+		proxywasm.ReplaceHttpRequestHeader("Cache-Control", "no-cache, no-store")
+
+		frontendVersion := ctx.GetContext(config.XPreHigressTag).(string)
+		backendVersion := ctx.GetContext(config.XMseTag).(string)
+
+		// 设置当前的前端版本
+		proxywasm.AddHttpResponseHeader("Set-Cookie", fmt.Sprintf("%s=%s; Path=/;", config.XPreHigressTag, frontendVersion))
+		// 设置后端的前端版本
+		proxywasm.AddHttpResponseHeader("Set-Cookie", fmt.Sprintf("%s=%s; Path=/;", config.XMseTag, backendVersion))
 	}
 	return types.ActionContinue
+}
+
+func onHttpResponseBody(ctx wrapper.HttpContext, grayConfig config.GrayConfig, body []byte, log wrapper.Log) types.Action {
+	if !util.IsGrayEnabled(grayConfig) {
+		return types.ActionContinue
+	}
+	backendVersion := ctx.GetContext(config.XMseTag)
+	isHtml := ctx.GetContext(config.IsHTML)
+	isIndex := ctx.GetContext(config.IsIndex)
+	notFoundUri := ctx.GetContext(config.NotFound)
+	if isIndex != nil && isIndex.(bool) && notFoundUri != nil && notFoundUri.(bool) && grayConfig.Rewrite.Host != "" && grayConfig.Rewrite.NotFound != "" {
+		client := wrapper.NewClusterClient(wrapper.RouteCluster{Host: grayConfig.Rewrite.Host})
+		client.Get(grayConfig.Rewrite.NotFound, nil, func(statusCode int, responseHeaders http.Header, responseBody []byte) {
+			proxywasm.ReplaceHttpResponseBody(responseBody)
+			proxywasm.ResumeHttpResponse()
+		}, 1500)
+		return types.ActionPause
+	}
+
+	// 以text/html 开头，将 cookie转到cookie
+	if isHtml != nil && isHtml.(bool) && backendVersion != nil && backendVersion.(string) != "" {
+		newText := strings.ReplaceAll(string(body), "</head>", `<script>
+				!function(e,t){function n(e){var n="; "+t.cookie,r=n.split("; "+e+"=");return 2===r.length?r.pop().split(";").shift():null}var r=n("x-mse-tag");if(!r)return null;var s=XMLHttpRequest.prototype.open;XMLHttpRequest.prototype.open=function(e,t,n,a,i){return this._XHR=!0,this.addEventListener("readystatechange",function(){1===this.readyState&&r&&this.setRequestHeader("x-mse-tag",r)}),s.apply(this,arguments)};var a=e.fetch;e.fetch=function(e,t){return"undefined"==typeof t&&(t={}),"undefined"==typeof t.headers&&(t.headers={}),r&&(t.headers["x-mse-tag"]=r),a.apply(this,[e,t])}}(window,document);
+			</script>
+		</head>`)
+		if err := proxywasm.ReplaceHttpResponseBody([]byte(newText)); err != nil {
+			return types.ActionContinue
+		}
+	}
+	return types.ActionContinue
+}
+
+func onStreamingResponseBody(ctx wrapper.HttpContext, pluginConfig config.GrayConfig, chunk []byte, isLastChunk bool, log wrapper.Log) []byte {
+	return chunk
 }
--- plugins/wasm-go/extensions/frontend-gray/util/utils.go
+++ plugins/wasm-go/extensions/frontend-gray/util/utils.go
@@ -2,45 +2,86 @@ package util
 
 import (
 	"net/url"
+	"path"
+	"path/filepath"
+	"sort"
 	"strings"
+
+	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
 
 	"github.com/alibaba/higress/plugins/wasm-go/extensions/frontend-gray/config"
 
 	"github.com/tidwall/gjson"
 )
 
-// GetValueByCookie 根据 cookieStr 和 cookieName 获取 cookie 值
-func GetValueByCookie(cookieStr string, cookieName string) string {
-	if cookieStr == "" {
+func IsGrayEnabled(grayConfig config.GrayConfig) bool {
+	// 检查是否存在重写主机
+	if grayConfig.Rewrite != nil && grayConfig.Rewrite.Host != "" {
+		return true
+	}
+
+	// 检查灰度部署是否为 nil 或空
+	grayDeployments := grayConfig.GrayDeployments
+	if grayDeployments != nil && len(grayDeployments) > 0 {
+		for _, grayDeployment := range grayDeployments {
+			if grayDeployment.Enabled {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// ExtractCookieValueByKey 根据 cookie 和 key 获取 cookie 值
+func ExtractCookieValueByKey(cookie string, key string) string {
+	if cookie == "" {
 		return ""
 	}
-	cookies := strings.Split(cookieStr, ";")
-	curCookieName := cookieName + "="
-	var foundCookieValue string
-	var found bool
-	// 遍历找到 cookie 对并处理
-	for _, cookie := range cookies {
-		cookie = strings.TrimSpace(cookie) // 清理空白符
-		if strings.HasPrefix(cookie, curCookieName) {
-			foundCookieValue = cookie[len(curCookieName):]
-			found = true
+	value := ""
+	pairs := strings.Split(cookie, ";")
+	for _, pair := range pairs {
+		pair = strings.TrimSpace(pair)
+		kv := strings.Split(pair, "=")
+		if kv[0] == key {
+			value = kv[1]
 			break
 		}
 	}
-	if !found {
-		return ""
-	}
-	return foundCookieValue
+	return value
 }
 
-// contains 检查切片 slice 中是否含有元素 value。
-func Contains(slice []interface{}, value string) bool {
+func ContainsValue(slice []string, value string) bool {
 	for _, item := range slice {
 		if item == value {
 			return true
 		}
 	}
 	return false
+}
+
+// headers: [][2]string -> map[string][]string
+func ConvertHeaders(hs [][2]string) map[string][]string {
+	ret := make(map[string][]string)
+	for _, h := range hs {
+		k, v := strings.ToLower(h[0]), h[1]
+		ret[k] = append(ret[k], v)
+	}
+	return ret
+}
+
+// headers: map[string][]string -> [][2]string
+func ReconvertHeaders(hs map[string][]string) [][2]string {
+	var ret [][2]string
+	for k, vs := range hs {
+		for _, v := range vs {
+			ret = append(ret, [2]string{k, v})
+		}
+	}
+	sort.SliceStable(ret, func(i, j int) bool {
+		return ret[i][0] < ret[j][0]
+	})
+	return ret
 }
 
 func GetRule(rules []*config.GrayRule, name string) *config.GrayRule {
@@ -52,7 +93,66 @@ func GetRule(rules []*config.GrayRule, name string) *config.GrayRule {
 	return nil
 }
 
-func GetBySubKey(grayInfoStr string, graySubKey string) string {
+// 检查是否是页面
+var indexSuffixes = []string{
+	".html", ".htm", ".jsp", ".php", ".asp", ".aspx", ".erb", ".ejs", ".twig",
+}
+
+// IsIndexRequest determines if the request is an index request
+func IsIndexRequest(fetchMode string, p string) bool {
+	if fetchMode == "cors" {
+		return false
+	}
+	ext := path.Ext(p)
+	return ext == "" || ContainsValue(indexSuffixes, ext)
+}
+
+// 首页Rewrite
+func IndexRewrite(path, version string, matchRules map[string]string) string {
+	for prefix, rewrite := range matchRules {
+		if strings.HasPrefix(path, prefix) {
+			newPath := strings.Replace(rewrite, "{version}", version, -1)
+			return newPath
+		}
+	}
+	return path
+}
+
+func PrefixFileRewrite(path, version string, matchRules map[string]string) string {
+	var matchedPrefix, replacement string
+	for prefix, template := range matchRules {
+		if strings.HasPrefix(path, prefix) {
+			if len(prefix) > len(matchedPrefix) { // 找到更长的前缀
+				matchedPrefix = prefix
+				replacement = strings.Replace(template, "{version}", version, 1)
+			}
+		}
+	}
+	// 将path 中的前缀部分用 replacement 替换掉
+	newPath := strings.Replace(path, matchedPrefix, replacement+"/", 1)
+	return filepath.Clean(newPath)
+}
+
+func GetVersion(version string, cookies string, isIndex bool) string {
+	if isIndex {
+		return version
+	}
+	// 来自Cookie中的版本
+	cookieVersion := ExtractCookieValueByKey(cookies, config.XPreHigressTag)
+	// cookie 中为空，返回当前版本
+	if cookieVersion == "" {
+		return version
+	}
+
+	// cookie 中和当前版本不相同，返回cookie中值
+	if cookieVersion != version {
+		return cookieVersion
+	}
+	return version
+}
+
+// 从cookie中解析出灰度信息
+func getBySubKey(grayInfoStr string, graySubKey string) string {
 	// 首先对 URL 编码的字符串进行解码
 	jsonStr, err := url.QueryUnescape(grayInfoStr)
 	if err != nil {
@@ -67,4 +167,44 @@ func GetBySubKey(grayInfoStr string, graySubKey string) string {
 	}
 	// 返回字符串形式的值
 	return value.String()
+}
+
+func GetGrayKey(grayKeyValue string, graySubKey string) string {
+	// 如果有子key, 尝试从子key中获取值
+	if graySubKey != "" {
+		subKeyValue := getBySubKey(grayKeyValue, graySubKey)
+		if subKeyValue != "" {
+			grayKeyValue = subKeyValue
+		}
+	}
+	return grayKeyValue
+}
+
+// FilterGrayRule 过滤灰度规则
+func FilterGrayRule(grayConfig *config.GrayConfig, grayKeyValue string, logInfof func(format string, args ...interface{})) *config.GrayDeployment {
+	for _, grayDeployment := range grayConfig.GrayDeployments {
+		if !grayDeployment.Enabled {
+			// 跳过Enabled=false
+			continue
+		}
+		grayRule := GetRule(grayConfig.Rules, grayDeployment.Name)
+		// 首先：先校验用户名单ID
+		if grayRule.GrayKeyValue != nil && len(grayRule.GrayKeyValue) > 0 && grayKeyValue != "" {
+			if ContainsValue(grayRule.GrayKeyValue, grayKeyValue) {
+				logInfof("frontendVersion: %s, grayKeyValue: %s", grayDeployment.Version, grayKeyValue)
+				return grayDeployment
+			}
+		}
+		//	第二：校验Cookie中的 GrayTagKey
+		if grayRule.GrayTagKey != "" && grayRule.GrayTagValue != nil && len(grayRule.GrayTagValue) > 0 {
+			cookieStr, _ := proxywasm.GetHttpRequestHeader("cookie")
+			grayTagValue := ExtractCookieValueByKey(cookieStr, grayRule.GrayTagKey)
+			if ContainsValue(grayRule.GrayTagValue, grayTagValue) {
+				logInfof("frontendVersion: %s, grayTag: %s=%s", grayDeployment.Version, grayRule.GrayTagKey, grayTagValue)
+				return grayDeployment
+			}
+		}
+	}
+	logInfof("frontendVersion: %s, grayKeyValue: %s", grayConfig.BaseDeployment.Version, grayKeyValue)
+	return nil
 }
