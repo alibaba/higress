29286b2a5252f4c1c0bf38138b50ae41af95d86e
rinfx <893383980@qq.com>
2024-09-23 20:05:26 +0800

    optimize

--- plugins/wasm-go/extensions/ai-security-guard/main.go
+++ plugins/wasm-go/extensions/ai-security-guard/main.go
@@ -33,23 +33,35 @@ func main() {
 }
 
 const (
-	NormalResponseFormat      = `{"id": "chatcmpl-123","object": "chat.completion","created": 1677652288,"model": "gpt-4o-mini","system_fingerprint": "fp_44709d6fcb","choices": [{"index": 0,"message": {"role": "assistant","content": "%s"},"logprobs": null,"finish_reason": "stop"}]}`
-	StreamResponseChunk       = `data:{"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{"role":"assistant","content":"%s"},"logprobs":null,"finish_reason":null}]}`
-	StreamResponseEnd         = `data:{"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}`
-	StreamResponseFormat      = StreamResponseChunk + "\n\n" + StreamResponseEnd
-	TracingPrefix             = "trace_span_tag."
-	DefaultResponseIfNoAdvice = "很抱歉，我不能对您的问题做出回答。"
+	OpenAIResponseFormat       = `{"id": "chatcmpl-123","object": "chat.completion","model": "gpt-4o-mini","choices": [{"index": 0,"message": {"role": "assistant","content": "%s"},"logprobs": null,"finish_reason": "stop"}]}`
+	OpenAIStreamResponseChunk  = `data:{"id":"chatcmpl-123","object":"chat.completion.chunk","model":"gpt-4o-mini", "choices":[{"index":0,"delta":{"role":"assistant","content":"%s"},"logprobs":null,"finish_reason":null}]}`
+	OpenAIStreamResponseEnd    = `data:{"id":"chatcmpl-123","object":"chat.completion.chunk","model":"gpt-4o-mini", "choices":[{"index":0,"delta":{},"logprobs":null,"finish_reason":"stop"}]}`
+	OpenAIStreamResponseFormat = OpenAIStreamResponseChunk + "\n\n" + OpenAIStreamResponseEnd
+
+	TracingPrefix = "trace_span_tag."
+
+	DefaultRequestCheckService       = "llm_query_moderation"
+	DefaultResponseCheckService      = "llm_response_moderation"
+	DefaultRequestJsonPath           = "messages.@reverse.0.content"
+	DefaultResponseJsonPath          = "choices.0.message.content"
+	DefaultStreamingResponseJsonPath = "choices.0.delta.content"
 )
 
 type AISecurityConfig struct {
-	client               wrapper.HttpClient
-	ak                   string
-	sk                   string
-	checkRequest         bool
-	requestCheckService  string
-	checkResponse        bool
-	responseCheckService string
-	metrics              map[string]proxywasm.MetricCounter
+	client                        wrapper.HttpClient
+	ak                            string
+	sk                            string
+	checkRequest                  bool
+	requestCheckService           string
+	requestContentJsonPath        string
+	checkResponse                 bool
+	responseCheckService          string
+	responseContentJsonPath       string
+	responseStreamContentJsonPath string
+	original                      bool
+	denyCode                      int64
+	denyMessage                   string
+	metrics                       map[string]proxywasm.MetricCounter
 }
 
 func (config *AISecurityConfig) incrementCounter(metricName string, inc uint64) {
@@ -104,6 +116,7 @@ func parseConfig(json gjson.Result, config *AISecurityConfig, log wrapper.Log) e
 	serviceName := json.Get("serviceName").String()
 	servicePort := json.Get("servicePort").Int()
 	serviceHost := json.Get("serviceHost").String()
+	config.original = json.Get("original").Bool()
 	if serviceName == "" || servicePort == 0 || serviceHost == "" {
 		return errors.New("invalid service config")
 	}
@@ -114,15 +127,36 @@ func parseConfig(json gjson.Result, config *AISecurityConfig, log wrapper.Log) e
 	}
 	config.checkRequest = json.Get("checkRequest").Bool()
 	config.checkResponse = json.Get("checkResponse").Bool()
-	if json.Get("requestCheckService").Exists() {
-		config.requestCheckService = json.Get("requestCheckService").String()
+	config.denyMessage = json.Get("denyMessage").String()
+	if obj := json.Get("denyCode"); obj.Exists() {
+		config.denyCode = obj.Int()
 	} else {
-		config.requestCheckService = "llm_query_moderation"
+		config.denyCode = 403
 	}
-	if json.Get("responseCheckService").Exists() {
-		config.responseCheckService = json.Get("responseCheckService").String()
+	if obj := json.Get("requestCheckService"); obj.Exists() {
+		config.requestCheckService = obj.String()
 	} else {
-		config.responseCheckService = "llm_response_moderation"
+		config.requestCheckService = DefaultRequestCheckService
+	}
+	if obj := json.Get("responseCheckService"); obj.Exists() {
+		config.responseCheckService = obj.String()
+	} else {
+		config.responseCheckService = DefaultResponseCheckService
+	}
+	if obj := json.Get("requestContentJsonPath"); obj.Exists() {
+		config.requestContentJsonPath = obj.String()
+	} else {
+		config.requestContentJsonPath = DefaultRequestJsonPath
+	}
+	if obj := json.Get("responseContentJsonPath"); obj.Exists() {
+		config.responseContentJsonPath = obj.String()
+	} else {
+		config.responseContentJsonPath = DefaultResponseJsonPath
+	}
+	if obj := json.Get("responseStreamContentJsonPath"); obj.Exists() {
+		config.responseStreamContentJsonPath = obj.String()
+	} else {
+		config.responseStreamContentJsonPath = DefaultStreamingResponseJsonPath
 	}
 	config.client = wrapper.NewClusterClient(wrapper.FQDNCluster{
 		FQDN: serviceName,
@@ -144,15 +178,9 @@ func onHttpRequestHeaders(ctx wrapper.HttpContext, config AISecurityConfig, log 
 }
 
 func onHttpRequestBody(ctx wrapper.HttpContext, config AISecurityConfig, body []byte, log wrapper.Log) types.Action {
-	messages := gjson.GetBytes(body, "messages").Array()
-	stream := gjson.GetBytes(body, "stream").Bool()
-	if len(messages) > 0 {
-		role := messages[len(messages)-1].Get("role").String()
-		content := messages[len(messages)-1].Get("content").String()
-		log.Debugf("Raw request content is: %s", content)
-		if role != "user" {
-			return types.ActionContinue
-		}
+	stream := !config.original && gjson.GetBytes(body, "stream").Bool()
+	content := gjson.GetBytes(body, config.requestContentJsonPath).String()
+	if content != "" {
 		timestamp := time.Now().UTC().Format("2006-01-02T15:04:05Z")
 		randomID, _ := generateHexID(16)
 		params := map[string]string{
@@ -182,26 +210,16 @@ func onHttpRequestBody(ctx wrapper.HttpContext, config AISecurityConfig, body []
 					if respAdvice.Exists() {
 						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_risklabel"}, []byte(respResult.Array()[0].Get("Label").String()))
 						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_deny_phase"}, []byte("request"))
-						if stream {
-							jsonData := []byte(fmt.Sprintf(StreamResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
-							config.incrementCounter("ai_sec_request_deny", 1)
-							proxywasm.SendHttpResponse(200, [][2]string{{"content-type", "text/event-stream;charset=UTF-8"}}, jsonData, -1)
+						config.incrementCounter("ai_sec_request_deny", 1)
+						if config.denyMessage != "" {
+							proxywasm.SendHttpResponse(uint32(config.denyCode), [][2]string{{"content-type", "application/json"}}, []byte(config.denyMessage), -1)
 						} else {
-							jsonData := []byte(fmt.Sprintf(NormalResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
-							config.incrementCounter("ai_sec_request_deny", 1)
-							proxywasm.SendHttpResponse(200, [][2]string{{"content-type", "application/json"}}, jsonData, -1)
-						}
-					} else if respResult.Array()[0].Get("Label").String() != "nonLabel" {
-						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_risklabel"}, []byte(respResult.Array()[0].Get("Label").String()))
-						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_deny_phase"}, []byte("request"))
-						if stream {
-							jsonData := []byte(fmt.Sprintf(StreamResponseFormat, DefaultResponseIfNoAdvice))
-							config.incrementCounter("ai_sec_request_deny", 1)
-							proxywasm.SendHttpResponse(200, [][2]string{{"content-type", "text/event-stream;charset=UTF-8"}}, jsonData, -1)
-						} else {
-							jsonData := []byte(fmt.Sprintf(NormalResponseFormat, DefaultResponseIfNoAdvice))
-							config.incrementCounter("ai_sec_request_deny", 1)
-							proxywasm.SendHttpResponse(200, [][2]string{{"content-type", "application/json"}}, jsonData, -1)
+							jsonData := []byte(fmt.Sprintf(OpenAIStreamResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
+							if stream {
+								proxywasm.SendHttpResponse(uint32(config.denyCode), [][2]string{{"content-type", "text/event-stream;charset=UTF-8"}}, jsonData, -1)
+							} else {
+								proxywasm.SendHttpResponse(uint32(config.denyCode), [][2]string{{"content-type", "application/json"}}, jsonData, -1)
+							}
 						}
 					} else {
 						proxywasm.ResumeHttpRequest()
@@ -252,7 +270,14 @@ func onHttpResponseHeaders(ctx wrapper.HttpContext, config AISecurityConfig, log
 }
 
 func onHttpResponseBody(ctx wrapper.HttpContext, config AISecurityConfig, body []byte, log wrapper.Log) types.Action {
-	content := extractResponseMessage(body)
+	hdsMap := ctx.GetContext("headers").(map[string][]string)
+	isStreamingResponse := strings.Contains(strings.Join(hdsMap["content-type"], ";"), "event-stream")
+	var content string
+	if isStreamingResponse {
+		content = extractMessageFromStreamingBody(body, config.responseStreamContentJsonPath)
+	} else {
+		content = gjson.GetBytes(body, config.responseContentJsonPath).String()
+	}
 	log.Debugf("Raw response content is: %s", content)
 	if len(content) > 0 {
 		timestamp := time.Now().UTC().Format("2006-01-02T15:04:05Z")
@@ -283,30 +308,18 @@ func onHttpResponseBody(ctx wrapper.HttpContext, config AISecurityConfig, body [
 					respAdvice := respData.Get("Advice")
 					respResult := respData.Get("Result")
 					if respAdvice.Exists() {
-						hdsMap := ctx.GetContext("headers").(map[string][]string)
 						var jsonData []byte
-						if strings.Contains(strings.Join(hdsMap["content-type"], ";"), "event-stream") {
-							jsonData = []byte(fmt.Sprintf(StreamResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
+						if config.denyMessage != "" {
+							jsonData = []byte(config.denyMessage)
 						} else {
-							jsonData = []byte(fmt.Sprintf(NormalResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
+							if strings.Contains(strings.Join(hdsMap["content-type"], ";"), "event-stream") {
+								jsonData = []byte(fmt.Sprintf(OpenAIStreamResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
+							} else {
+								jsonData = []byte(fmt.Sprintf(OpenAIResponseFormat, respAdvice.Array()[0].Get("Answer").String()))
+							}
 						}
 						delete(hdsMap, "content-length")
-						hdsMap[":status"] = []string{"200"}
-						proxywasm.ReplaceHttpResponseHeaders(reconvertHeaders(hdsMap))
-						proxywasm.ReplaceHttpResponseBody(jsonData)
-						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_risklabel"}, []byte(respResult.Array()[0].Get("Label").String()))
-						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_deny_phase"}, []byte("response"))
-						config.incrementCounter("ai_sec_response_deny", 1)
-					} else if respResult.Array()[0].Get("Label").String() != "nonLabel" {
-						hdsMap := ctx.GetContext("headers").(map[string][]string)
-						var jsonData []byte
-						if strings.Contains(strings.Join(hdsMap["content-type"], ";"), "event-stream") {
-							jsonData = []byte(fmt.Sprintf(StreamResponseFormat, DefaultResponseIfNoAdvice))
-						} else {
-							jsonData = []byte(fmt.Sprintf(NormalResponseFormat, DefaultResponseIfNoAdvice))
-						}
-						delete(hdsMap, "content-length")
-						hdsMap[":status"] = []string{"200"}
+						hdsMap[":status"] = []string{fmt.Sprint(config.denyCode)}
 						proxywasm.ReplaceHttpResponseHeaders(reconvertHeaders(hdsMap))
 						proxywasm.ReplaceHttpResponseBody(jsonData)
 						proxywasm.SetProperty([]string{TracingPrefix, "ai_sec_risklabel"}, []byte(respResult.Array()[0].Get("Label").String()))
@@ -322,18 +335,12 @@ func onHttpResponseBody(ctx wrapper.HttpContext, config AISecurityConfig, body [
 	}
 }
 
-func extractResponseMessage(data []byte) string {
-	// try to get llm-response
-	contentObj := gjson.GetBytes(data, "choices.0.message.content")
-	if contentObj.Exists() {
-		return contentObj.String()
-	}
-	// try to get llm-stream-response
+func extractMessageFromStreamingBody(data []byte, jsonPath string) string {
 	chunks := bytes.Split(bytes.TrimSpace(data), []byte("\n\n"))
 	strChunks := []string{}
 	for _, chunk := range chunks {
 		// Example: "choices":[{"index":0,"delta":{"role":"assistant","content":"%s"},"logprobs":null,"finish_reason":null}]
-		jsonObj := gjson.GetBytes(chunk, "choices.0.delta.content")
+		jsonObj := gjson.GetBytes(chunk, jsonPath)
 		if jsonObj.Exists() {
 			strChunks = append(strChunks, jsonObj.String())
 		}
