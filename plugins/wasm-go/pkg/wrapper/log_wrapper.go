b33c38f0311e88fa860071c7c0339c5c8df48612
rinfx <893383980@qq.com>
2024-11-04 20:06:11 +0800

    update

--- plugins/wasm-go/pkg/wrapper/log_wrapper.go
+++ plugins/wasm-go/pkg/wrapper/log_wrapper.go
@@ -36,8 +36,12 @@ type Log struct {
 }
 
 func (l Log) log(level LogLevel, msg string) {
-	requestID, _ := proxywasm.GetProperty([]string{"x_request_id"})
-	msg = fmt.Sprintf("[%s] [%s] %s", l.pluginName, string(requestID), msg)
+	requestIDRaw, _ := proxywasm.GetProperty([]string{"x_request_id"})
+	requestID := string(requestIDRaw)
+	if requestID == "" {
+		requestID = "nil"
+	}
+	msg = fmt.Sprintf("[%s] [%s] %s", l.pluginName, requestID, msg)
 	switch level {
 	case LogLevelTrace:
 		proxywasm.LogTrace(msg)
@@ -55,7 +59,12 @@ func (l Log) log(level LogLevel, msg string) {
 }
 
 func (l Log) logFormat(level LogLevel, format string, args ...interface{}) {
-	format = fmt.Sprintf("[%s] %s", l.pluginName, format)
+	requestIDRaw, _ := proxywasm.GetProperty([]string{"x_request_id"})
+	requestID := string(requestIDRaw)
+	if requestID == "" {
+		requestID = "nil"
+	}
+	format = fmt.Sprintf("[%s] [%s] %s", l.pluginName, requestID, format)
 	switch level {
 	case LogLevelTrace:
 		proxywasm.LogTracef(format, args...)
