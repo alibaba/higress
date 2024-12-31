5f312ccb1ac48a5d3457f72925e1ffda3081889d
daijingze_mac <18373118@buaa.edu.cn>
2024-12-31 15:35:18 +0800

    remove unused files

--- plugins/wasm-go/extensions/oidc/upload.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/daijingze/GolandProjects/github/higress/plugins/wasm-go
-PLUGIN_NAME=oidc make build
-cd extensions/oidc/
-docker build -t oidc-wasm .
-image_id=$(docker images | grep "^oidc-wasm " | awk '{print $3}')
-docker tag $image_id registry.cn-hangzhou.aliyuncs.com/jingze/oidc:1.0.0
-docker push registry.cn-hangzhou.aliyuncs.com/jingze/oidc:1.0.0
