8792448a2bcac7c27cba3ddba86cd8fdd9c36223
hanxiantao <601803023@qq.com>
2025-02-09 14:54:20 +0800

    add TestCaseName

--- test/e2e/conformance/tests/replay-protection.go
+++ test/e2e/conformance/tests/replay-protection.go
@@ -1,17 +1,3 @@
-// Copyright (c) 2022 Alibaba Group Holding Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package tests
 
 import (
@@ -43,14 +29,15 @@ var WasmPluginsReplayProtection = suite.ConformanceTest{
 		testcases := []http.Assertion{
 			{
 				Meta: http.AssertionMeta{
+					TestCaseName:    "Missing nonce header",
 					TargetBackend:   "infra-backend-v1",
 					TargetNamespace: "higress-conformance-infra",
 				},
 				Request: http.AssertionRequest{
 					ActualRequest: http.Request{
-						Host:        "foo.com",
-						Path:        "/",
-						Method:      "GET",
+						Host:   "foo.com",
+						Path:   "/",
+						Method: "GET",
 					},
 				},
 				Response: http.AssertionResponse{
@@ -61,6 +48,7 @@ var WasmPluginsReplayProtection = suite.ConformanceTest{
 			},
 			{
 				Meta: http.AssertionMeta{
+					TestCaseName:    "Invalid nonce not base64 encoded",
 					TargetBackend:   "infra-backend-v1",
 					TargetNamespace: "higress-conformance-infra",
 				},
@@ -82,6 +70,7 @@ var WasmPluginsReplayProtection = suite.ConformanceTest{
 			},
 			{
 				Meta: http.AssertionMeta{
+					TestCaseName:    "First request with unique nonce returns 200",
 					TargetBackend:   "infra-backend-v1",
 					TargetNamespace: "higress-conformance-infra",
 				},
@@ -103,6 +92,7 @@ var WasmPluginsReplayProtection = suite.ConformanceTest{
 			},
 			{
 				Meta: http.AssertionMeta{
+					TestCaseName:    "Second request with repeated nonce returns 429",
 					TargetBackend:   "infra-backend-v1",
 					TargetNamespace: "higress-conformance-infra",
 				},
