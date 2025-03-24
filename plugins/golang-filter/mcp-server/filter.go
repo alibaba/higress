e4a5ec6ae2348435413973fec1de63063e3deac1
Jingze <52855280+Jing-ze@users.noreply.github.com>
2025-03-24 17:52:16 +0800

    feat: add config parse in mcp server  (#1944)

--- plugins/golang-filter/mcp-server/filter.go
+++ plugins/golang-filter/mcp-server/filter.go
@@ -17,9 +17,10 @@ type filter struct {
 	path      string
 	config    *config
 
-	req     *http.Request
-	sse     bool
-	message bool
+	req        *http.Request
+	sse        bool
+	message    bool
+	bodyBuffer []byte
 }
 
 // Callbacks which are called in request path
@@ -29,38 +30,39 @@ func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.
 	parsedURL, _ := url.Parse(fullPath)
 	f.path = parsedURL.Path
 	method, _ := header.Get(":method")
-	api.LogInfo(f.path)
-	if f.path == f.config.SSEServer.SSEEndpoint {
-		if method != http.MethodGet {
-			f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusMethodNotAllowed, "Method not allowed", nil, 0, "")
-		} else {
-			f.sse = true
-			body := "SSE connection create"
-			f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusOK, body, nil, 0, "")
-		}
-		api.LogInfo("SSE connection started")
-		return api.LocalReply
-	} else if f.path == f.config.SSEServer.MessageEndpoint {
-		if method != http.MethodPost {
-			f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusMethodNotAllowed, "Method not allowed", nil, 0, "")
-		}
-		// Create a new http.Request object
-		f.req = &http.Request{
-			Method: method,
-			URL:    parsedURL,
-			Header: make(http.Header),
-		}
-		api.LogInfof("Message request: %v", parsedURL)
-		// Copy headers from api.RequestHeaderMap to http.Header
-		header.Range(func(key, value string) bool {
-			f.req.Header.Add(key, value)
-			return true
-		})
-		f.message = true
-		if endStream {
-			return api.Continue
-		} else {
-			return api.StopAndBuffer
+	for _, server := range f.config.servers {
+		if f.path == server.GetSSEEndpoint() {
+			if method != http.MethodGet {
+				f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusMethodNotAllowed, "Method not allowed", nil, 0, "")
+			} else {
+				f.sse = true
+				body := "SSE connection create"
+				f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusOK, body, nil, 0, "")
+			}
+			api.LogInfof("%s SSE connection started", server.GetServerName())
+			return api.LocalReply
+		} else if f.path == server.GetMessageEndpoint() {
+			if method != http.MethodPost {
+				f.callbacks.DecoderFilterCallbacks().SendLocalReply(http.StatusMethodNotAllowed, "Method not allowed", nil, 0, "")
+			}
+			// Create a new http.Request object
+			f.req = &http.Request{
+				Method: method,
+				URL:    parsedURL,
+				Header: make(http.Header),
+			}
+			api.LogDebugf("Message request: %v", parsedURL)
+			// Copy headers from api.RequestHeaderMap to http.Header
+			header.Range(func(key, value string) bool {
+				f.req.Header.Add(key, value)
+				return true
+			})
+			f.message = true
+			if endStream {
+				return api.Continue
+			} else {
+				return api.StopAndBuffer
+			}
 		}
 	}
 	if endStream {
@@ -73,18 +75,25 @@ func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.
 // DecodeData might be called multiple times during handling the request body.
 // The endStream is true when handling the last piece of the body.
 func (f *filter) DecodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
-	api.LogInfo("Message DecodeData")
-	// support suspending & resuming the filter in a background goroutine
-	api.LogInfof("DecodeData: {%v}", buffer)
 	if f.message {
-		// Create a response recorder to capture the response
-		recorder := httptest.NewRecorder()
-		// Call the handleMessage method of SSEServer
-		f.config.SSEServer.HandleMessage(recorder, f.req, buffer.Bytes())
-		f.message = false
-		api.LogInfof("Message DecodeData SendLocalReply %v", recorder)
-		f.callbacks.DecoderFilterCallbacks().SendLocalReply(recorder.Code, recorder.Body.String(), recorder.Header(), 0, "")
-		return api.LocalReply
+		f.bodyBuffer = append(f.bodyBuffer, buffer.Bytes()...)
+
+		if endStream {
+			for _, server := range f.config.servers {
+				if f.path == server.GetMessageEndpoint() {
+					// Create a response recorder to capture the response
+					recorder := httptest.NewRecorder()
+					// Call the handleMessage method of SSEServer with complete body
+					server.HandleMessage(recorder, f.req, f.bodyBuffer)
+					f.message = false
+					// clear buffer
+					f.bodyBuffer = nil
+					f.callbacks.DecoderFilterCallbacks().SendLocalReply(recorder.Code, recorder.Body.String(), recorder.Header(), 0, "")
+					return api.LocalReply
+				}
+			}
+		}
+		return api.StopAndBuffer
 	}
 	return api.Continue
 }
@@ -98,23 +107,21 @@ func (f *filter) EncodeHeaders(header api.ResponseHeaderMap, endStream bool) api
 		header.Set("Connection", "keep-alive")
 		header.Set("Access-Control-Allow-Origin", "*")
 		header.Del("Content-Length")
-		api.LogInfo("SSE connection header set")
 		return api.Continue
 	}
 	return api.Continue
 }
 
-// TODO: 连接多种数据库
-// TODO: 多种存储类型
-// TODO: 数据库多个实例
 // EncodeData might be called multiple times during handling the response body.
 // The endStream is true when handling the last piece of the body.
 func (f *filter) EncodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
-	if f.sse {
-		//TODO: buffer cleanup
-		f.config.SSEServer.HandleSSE(f.callbacks)
-		f.sse = false
-		return api.Running
+	for _, server := range f.config.servers {
+		if f.sse {
+			buffer.Reset()
+			server.HandleSSE(f.callbacks)
+			f.sse = false
+			return api.Running
+		}
 	}
 	return api.Continue
 }
