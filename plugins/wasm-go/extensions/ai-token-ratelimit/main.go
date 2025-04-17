99976fda9ca31c93de6a5e263292789d41faa6b2
zhihao02.huang <zhihao02.huang@vipshop.com>
2025-04-17 17:55:25 +0800

    fix ai-token-ratelimit plugin

--- plugins/wasm-go/extensions/ai-token-ratelimit/main.go
+++ plugins/wasm-go/extensions/ai-token-ratelimit/main.go
@@ -140,7 +140,7 @@ func onHttpRequestHeaders(ctx wrapper.HttpContext, config ClusterKeyRateLimitCon
 		log.Errorf("redis call failed: %v", err)
 		return types.ActionContinue
 	}
-	return types.ActionPause
+	return types.HeaderStopAllIterationAndWatermark
 }
 
 func onHttpStreamingBody(ctx wrapper.HttpContext, config ClusterKeyRateLimitConfig, data []byte, endOfStream bool, log wrapper.Log) []byte {
