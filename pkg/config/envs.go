c7fe2706d1875be066203e4ba849eb67b930dea9
Erica177 <1334766991@qq.com>
2025-04-27 11:11:55 +0800

    add wasm env

--- pkg/config/envs.go
+++ pkg/config/envs.go
@@ -23,5 +23,5 @@ var (
 	// Revision is the value of the Istio control plane revision, e.g. "canary",
 	// and is the value used by the "istio.io/rev" label.
 	Revision              = env.Register("REVISION", "", "").Get()
-	McpServerWasmImageUrl = env.RegisterStringVar("MCP_SERVER_WASM_IMAGE_URL", "", "").Get()
+	McpServerWasmImageUrl = env.RegisterStringVar("MCP_SERVER_WASM_IMAGE_URL", "oci://higress-registry.cn-hangzhou.cr.aliyuncs.com/mcp-server/all-in-one:1.0.0", "").Get()
 )
