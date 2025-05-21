426b56ceea55aee91fbabbc38f891dceadf6b88f
Rishi Mondal <mavrickrishi@gmail.com>
2025-05-21 16:24:24 +0530

    fix: update Docker build process to use minimal variant as default and clean up Dockerfile references

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -137,9 +137,10 @@ endif
 # for now docker is limited to Linux compiles - why ?
 include docker/docker.mk
 
-docker-build: DOCKER_BUILD_VARIANTS=minimal docker.higress ## Build and push docker images to registry defined by $HUB and $TAG
 
-docker-buildx-push: clean-env DOCKER_BUILD_VARIANTS=minimal docker.higress-buildx ## Build and push docker images to registry defined by $HUB and $TAG
+docker-build:  docker.higress ## Build and push docker images to registry defined by $HUB and $TAG
+
+docker-buildx-push: clean-env  docker.higress-buildx ## Build and push docker images to registry defined by $HUB and $TAG
 
 export PARENT_GIT_TAG:=$(shell cat VERSION)
 export PARENT_GIT_REVISION:=$(TAG)
