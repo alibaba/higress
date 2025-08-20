e68d0a6ddd0eeafbc16fbb8221c38dc4d6ceadf2
jingze <daijingze.djz@alibaba-inc.com>
2025-08-20 17:11:01 +0800

    update ai-proxy main_test.go

--- plugins/wasm-go/extensions/ai-proxy/main_test.go
+++ plugins/wasm-go/extensions/ai-proxy/main_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/alibaba/higress/plugins/wasm-go/extensions/ai-proxy/provider"
+	"github.com/alibaba/higress/plugins/wasm-go/extensions/ai-proxy/test"
 )
 
 func Test_getApiName(t *testing.T) {
@@ -56,4 +57,41 @@ func Test_getApiName(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAi360(t *testing.T) {
+	test.RunAi360ParseConfigTests(t)
+	test.RunAi360OnHttpRequestHeadersTests(t)
+	test.RunAi360OnHttpRequestBodyTests(t)
+	test.RunAi360OnHttpResponseHeadersTests(t)
+	test.RunAi360OnHttpResponseBodyTests(t)
+	test.RunAi360OnStreamingResponseBodyTests(t)
+}
+
+func TestOpenAI(t *testing.T) {
+	test.RunOpenAIParseConfigTests(t)
+	test.RunOpenAIOnHttpRequestHeadersTests(t)
+	test.RunOpenAIOnHttpRequestBodyTests(t)
+	test.RunOpenAIOnHttpResponseHeadersTests(t)
+	test.RunOpenAIOnHttpResponseBodyTests(t)
+	test.RunOpenAIOnStreamingResponseBodyTests(t)
+}
+
+func TestQwen(t *testing.T) {
+	test.RunQwenParseConfigTests(t)
+	test.RunQwenOnHttpRequestHeadersTests(t)
+	test.RunQwenOnHttpRequestBodyTests(t)
+	test.RunQwenOnHttpResponseHeadersTests(t)
+	test.RunQwenOnHttpResponseBodyTests(t)
+	test.RunQwenOnStreamingResponseBodyTests(t)
+}
+
+func TestGemini(t *testing.T) {
+	test.RunGeminiParseConfigTests(t)
+	test.RunGeminiOnHttpRequestHeadersTests(t)
+	test.RunGeminiOnHttpRequestBodyTests(t)
+	test.RunGeminiOnHttpResponseHeadersTests(t)
+	test.RunGeminiOnHttpResponseBodyTests(t)
+	test.RunGeminiOnStreamingResponseBodyTests(t)
+	test.RunGeminiGetImageURLTests(t)
 }
