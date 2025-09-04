55673d4ec0f3161a090bdc09aa3929c9e542df59
liuhy <liuhongyu@apache.org>
2025-09-04 10:41:49 +0800

    feat(gzip): add gzip configuration support and update default settings

--- pkg/ingress/kube/configmap/gzip_test.go
+++ pkg/ingress/kube/configmap/gzip_test.go
@@ -375,7 +375,7 @@ func TestGzipController_AddOrUpdateHigressConfig(t *testing.T) {
 			new: &HigressConfig{
 				Gzip: &Gzip{
 					Enable:              true,
-					MinContentLength:    1024,
+					MinContentLength:    2048, // Changed from 1024 to make it different from default
 					ContentType:         []string{"text/html", "text/css", "text/plain", "text/xml", "application/json", "application/javascript", "application/xhtml+xml", "image/svg+xml"},
 					DisableOnEtagHeader: true,
 					MemoryLevel:         5,
@@ -389,7 +389,7 @@ func TestGzipController_AddOrUpdateHigressConfig(t *testing.T) {
 			wantEventPush: "push",
 			wantGzip: &Gzip{
 				Enable:              true,
-				MinContentLength:    1024,
+				MinContentLength:    2048, // Changed from 1024 to make it different from default
 				ContentType:         []string{"text/html", "text/css", "text/plain", "text/xml", "application/json", "application/javascript", "application/xhtml+xml", "image/svg+xml"},
 				DisableOnEtagHeader: true,
 				MemoryLevel:         5,
