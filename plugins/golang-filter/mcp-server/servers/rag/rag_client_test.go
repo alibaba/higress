f4d5701fb7d45096efdd8cc4d49031111ed7032c
Jun <108045855+2456868764@users.noreply.github.com>
2025-09-23 14:03:00 +0800

    fix: llm can be empty and optimize document and prompt (#2942)

--- plugins/golang-filter/mcp-server/servers/rag/rag_client_test.go
+++ plugins/golang-filter/mcp-server/servers/rag/rag_client_test.go
@@ -1,6 +1,8 @@
 package rag
 
 import (
+	"encoding/json"
+	"os"
 	"testing"
 
 	"github.com/alibaba/higress/plugins/golang-filter/mcp-server/servers/rag/config"
@@ -137,7 +139,11 @@ func TestRAGClient_Chat(t *testing.T) {
 		t.Errorf("getRAGClient() error = %v", err)
 		return
 	}
-	query := "what is the competition about?"
+	// query := "Who is the individual associated with the cryptocurrency industry facing a criminal trial on fraud and conspiracy charges, as reported by both The Verge and TechCrunch, and is accused by prosecutors of committing fraud for personal gain?"
+	// query := "Which individual is implicated in both inflating the value of a Manhattan apartment to a figure not yet achieved in New York City's real estate history, according to 'Fortune', and is also accused of adjusting this apartment's valuation to compensate for a loss in another asset's worth, as reported by 'The Age'?"
+	// query := "Who is the figure associated with generative AI technology whose departure from OpenAI was considered shocking according to Fortune, and is also the subject of a prevailing theory suggesting a lack of full truthfulness with the board as reported by TechCrunch?"
+	// query := "Do the TechCrunch article on software companies and the Hacker News article on The Epoch Times both report an increase in revenue related to payment and subscription models, respectively?"
+	query := "Which online betting platform provides a welcome bonus of up to $1000 in bonus bets for new customers' first losses, runs NBA betting promotions, and is anticipated to extend the same sign-up offer to new users in Vermont, as reported by both CBSSports.com and Sporting News?"
 	resp, err := ragClient.Chat(query)
 	if err != nil {
 		t.Errorf("Chat() error = %v", err)
@@ -147,4 +153,45 @@ func TestRAGClient_Chat(t *testing.T) {
 		t.Errorf("Chat() resp = %s, want not empty", resp)
 		return
 	}
+	t.Logf("Chat() resp = %s", resp)
+}
+
+func TestRAGClient_LoadChunks(t *testing.T) {
+	t.Logf("TestRAGClient_LoadChunks")
+	ragClient, err := getRAGClient()
+	if err != nil {
+		t.Errorf("getRAGClient() error = %v", err)
+		return
+	}
+	// load json output/corpus.json and then call ragclient CreateChunkFromText to insert chunks
+	file, err := os.Open("/dataset/corpus.json")
+	if err != nil {
+		t.Errorf("LoadData() error = %v", err)
+		return
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	var data []struct {
+		Body  string `json:"body"`
+		Title string `json:"title"`
+		Url   string `json:"url"`
+	}
+	if err := decoder.Decode(&data); err != nil {
+		t.Errorf("LoadData() error = %v", err)
+		return
+	}
+
+	for _, item := range data {
+		t.Logf("LoadData() url = %s", item.Url)
+		t.Logf("LoadData() title = %s", item.Title)
+		t.Logf("LoadData() len body = %d", len(item.Body))
+		chunks, err := ragClient.CreateChunkFromText(item.Body, item.Title)
+		if err != nil {
+			t.Errorf("LoadData() error = %v", err)
+			continue
+		} else {
+			t.Logf("LoadData() chunks len = %d", len(chunks))
+		}
+	}
+	t.Logf("TestRAGClient_LoadChunks done")
 }
