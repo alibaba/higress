161b46d5c365d886e76184f73b957970ca6af01b
EndlessSeeker <1766508902@qq.com>
2025-11-19 10:58:09 +0800

    debug: e2e test
    Change-Id: Ie3cb0045c7c48a3f12d87af09b3b9af0e7d44eea

--- test/e2e/conformance/tests/httproute-downstream-encryption.go
+++ test/e2e/conformance/tests/httproute-downstream-encryption.go
@@ -15,19 +15,14 @@
 package tests
 
 import (
-	"bytes"
-	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
-	"testing"
-	"time"
-
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
 	"github.com/alibaba/higress/v2/test/e2e/conformance/utils/cert"
 	"github.com/alibaba/higress/v2/test/e2e/conformance/utils/http"
 	"github.com/alibaba/higress/v2/test/e2e/conformance/utils/kubernetes"
 	"github.com/alibaba/higress/v2/test/e2e/conformance/utils/suite"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
 )
 
 func init() {
@@ -36,34 +31,43 @@ func init() {
 
 // Shared certificates for use in both PreApplyHook and Test
 // This is necessary because certificates need to be generated once and used in both places
-var (
-	sharedCAKey      *rsa.PrivateKey
-	sharedCACertOut  *bytes.Buffer
-	sharedCliCertOut *bytes.Buffer
-	sharedCliKeyOut  *bytes.Buffer
-	sharedSvcCertOut *bytes.Buffer
-	sharedSvcKeyOut  *bytes.Buffer
-)
+//var (
+//	sharedCAKey      *rsa.PrivateKey
+//	sharedCACertOut  *bytes.Buffer
+//	sharedCliCertOut *bytes.Buffer
+//	sharedCliKeyOut  *bytes.Buffer
+//	sharedSvcCertOut *bytes.Buffer
+//	sharedSvcKeyOut  *bytes.Buffer
+//)
 
 var HTTPRouteDownstreamEncryption = suite.ConformanceTest{
 	ShortName:   "HTTPRouteDownstreamEncryption",
 	Description: "A single Ingress in the higress-conformance-infra namespace for downstream encryption.",
 	Manifests:   []string{"tests/httproute-downstream-encryption.yaml"},
 	Features:    []suite.SupportedFeature{suite.HTTPConformanceFeature},
-	PreApplyHook: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+	//PreApplyHook: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+	//	// Prepare certificates and secrets to be created BEFORE manifests are applied
+	//	// This ensures secrets exist when the Ingress resources reference them
+	//	var caCert *x509.Certificate
+	//	sharedCACertOut, _, caCert, sharedCAKey = cert.MustGenerateCaCert(t)
+	//	sharedSvcCertOut, sharedSvcKeyOut = cert.MustGenerateCertWithCA(t, cert.ServerCertType, caCert, sharedCAKey, []string{"foo.com"})
+	//	sharedCliCertOut, sharedCliKeyOut = cert.MustGenerateCertWithCA(t, cert.ClientCertType, caCert, sharedCAKey, nil)
+	//	fooSecret := kubernetes.ConstructTLSSecret("higress-conformance-infra", "foo-secret", sharedSvcCertOut.Bytes(), sharedSvcKeyOut.Bytes())
+	//	fooSecretCACert := kubernetes.ConstructCASecret("higress-conformance-infra", "foo-secret-cacert", sharedCACertOut.Bytes())
+	//	suite.Applier.MustApplyObjectsWithCleanup(t, suite.Client, suite.TimeoutConfig, []client.Object{fooSecret, fooSecretCACert}, suite.Cleanup)
+	//	time.Sleep(time.Second * 5)
+	//},
+	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+		// Use the certificates that were already generated in PreApplyHook
 		// Prepare certificates and secrets to be created BEFORE manifests are applied
 		// This ensures secrets exist when the Ingress resources reference them
 		var caCert *x509.Certificate
-		sharedCACertOut, _, caCert, sharedCAKey = cert.MustGenerateCaCert(t)
-		sharedSvcCertOut, sharedSvcKeyOut = cert.MustGenerateCertWithCA(t, cert.ServerCertType, caCert, sharedCAKey, []string{"foo.com"})
-		sharedCliCertOut, sharedCliKeyOut = cert.MustGenerateCertWithCA(t, cert.ClientCertType, caCert, sharedCAKey, nil)
+		sharedCACertOut, _, caCert, sharedCAKey := cert.MustGenerateCaCert(t)
+		sharedSvcCertOut, sharedSvcKeyOut := cert.MustGenerateCertWithCA(t, cert.ServerCertType, caCert, sharedCAKey, []string{"foo.com"})
+		sharedCliCertOut, sharedCliKeyOut := cert.MustGenerateCertWithCA(t, cert.ClientCertType, caCert, sharedCAKey, nil)
 		fooSecret := kubernetes.ConstructTLSSecret("higress-conformance-infra", "foo-secret", sharedSvcCertOut.Bytes(), sharedSvcKeyOut.Bytes())
 		fooSecretCACert := kubernetes.ConstructCASecret("higress-conformance-infra", "foo-secret-cacert", sharedCACertOut.Bytes())
 		suite.Applier.MustApplyObjectsWithCleanup(t, suite.Client, suite.TimeoutConfig, []client.Object{fooSecret, fooSecretCACert}, suite.Cleanup)
-		time.Sleep(time.Second * 5)
-	},
-	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
-		// Use the certificates that were already generated in PreApplyHook
 
 		testcases := []http.Assertion{
 			{
