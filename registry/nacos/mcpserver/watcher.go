f376a2b673a27b27ee82d80939123fba56b5a2b4
aias00 <liuhongyu@apache.org>
2026-01-15 10:46:21 +0800

    fix: skip unhealthy or disabled services form nacos and always marshal `AllowTools` field (#3220)
    Co-authored-by: EricaLiu <30773688+Erica177@users.noreply.github.com>

--- registry/nacos/mcpserver/watcher.go
+++ registry/nacos/mcpserver/watcher.go
@@ -434,7 +434,7 @@ func (w *watcher) processToolConfig(dataId, data string, credentials map[string]
 		rule.Server.SecuritySchemes = toolsDescription.SecuritySchemes
 	}
 
-	var allowTools []string
+	allowTools := []string{}
 	for _, t := range toolsDescription.Tools {
 		convertTool := &provider.McpTool{Name: t.Name, Description: t.Description}
 
@@ -813,6 +813,9 @@ func generateServiceEntry(host string, services *model.Service) *v1alpha3.Servic
 	endpoints := make([]*v1alpha3.WorkloadEntry, 0)
 
 	for _, service := range services.Hosts {
+		if !service.Healthy || !service.Enable {
+			continue
+		}
 		protocol := common.HTTP
 		if service.Metadata != nil && service.Metadata["protocol"] != "" {
 			protocol = common.ParseProtocol(service.Metadata["protocol"])
