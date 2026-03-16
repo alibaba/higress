8961db2e90d7451a8508d23f40e5c370a3185c18
woody <yaodiwu618@gmail.com>
2026-03-16 21:13:18 +0800

    fix: vertex streaming chunk parse (#3599)

--- plugins/wasm-go/extensions/ai-proxy/provider/provider.go
+++ plugins/wasm-go/extensions/ai-proxy/provider/provider.go
@@ -1046,7 +1046,7 @@ func ExtractStreamingEvents(ctx wrapper.HttpContext, chunk []byte) []StreamEvent
 		if lineStartIndex != -1 {
 			value := string(body[valueStartIndex:i])
 			currentEvent.SetValue(currentKey, value)
-		} else {
+		} else if eventStartIndex != -1 {
 			currentEvent.RawEvent = string(body[eventStartIndex : i+1])
 			// Extra new line. The current event is complete.
 			events = append(events, *currentEvent)
